38cdd942024a2ae43f649005e546c46fb3f5b099
jon12156 <32025765+jon12156@users.noreply.github.com>
2018-04-21 12:20:11 -0600

    use localhost instead of burke.ro

--- mainloop.lua
+++ mainloop.lua
@@ -98,7 +98,7 @@ do
     local items = {{"1P endless", main_select_speed_99, {main_endless}},
         {"1P puzzle", main_select_puzz},
         {"1P time attack", main_select_speed_99, {main_time_attack}},
-        {"2P fakevs at burke.ro", main_net_vs_setup, {"burke.ro"}},
+        {"2P fakevs at localhost", main_net_vs_setup, {"localhost"}},
         {"2P fakevs local game", main_local_vs_setup},
         {"Replay of 1P endless", main_replay_endless},
         {"Replay of 1P puzzle", main_replay_puzzle},
