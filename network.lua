66087fea26e91679d7d19a70c6663de2eba28cdd
Jon Willahan <32025765+jon12156@users.noreply.github.com>
2018-07-05 00:27:34 -0600

    bump version to 021

--- network.lua
+++ network.lua
@@ -108,7 +108,7 @@ function network_init(ip)
   end
   TCP_sock:settimeout(0)
   got_H = false
-  net_send("H020")
+  net_send("H021")
   assert(config.name and config.level and config.character)
   json_send({name=config.name, level=config.level, character=config.character})
 end
