5b837ff5957050c300bff2cdb11685de7ec0f4c5
jon12156 <32025765+jon12156@users.noreply.github.com>
2018-07-27 01:10:42 -0600

    rearranging sound files and adding find_sound fucntions
    the game is broken after this update, but I need to commit so I can go to bed and pick up from potentially a different computer tomorrow (hopefully)

--- sound.lua
+++ sound.lua
@@ -1,6 +1,5 @@
 --sets the volume of a single source or table of sources
 supported_sound_formats = {".ogg",".mp3", ".it"}
-
 function set_volume(source, new_volume)
   print("set_volume called")
   if type(source) == "table" then
@@ -16,13 +15,66 @@ function load_char_SFX(path_and_filename)
   local lfs = love.filesystem
   for k, char_name in (characters) do
     if lfs.isFile("sounds/"..sounds_dir.."/"..sounds_dir.."/characters/"..char_name.."/"..path_and_filename) then
-    
+    --TODO this whole function
     end
   end
   if lfs.isFile("sounds/"..sounds_dir.."/") then
   elseif lfs.isFile("sounds/"..sounds_dir.."/"..sounds_dir) then
   elseif lfs.isFile("sounds/"..sounds_dir.."/"..default_sounds_dir) then
   end
+end
+
+-- returns a new sound effect if it can be found, else returns nil
+function find_sound(sound_name, dirs_to_check)
+  local found_file
+  for k,dir in ipairs(dirs_to_check) do
+    found_file = check_supported_extensions(dir..sound_name)
+    if found_file then
+      return love.audio.newSource(found_file, static)
+    end
+  end
+  return nil
+end
+
+function find_generic_SFX(SFX_name)
+  local dirs_to_check = {"sounds/"..sounds_dir.."/SFX/",
+                         "sounds/"..default_sounds_dir.."/SFX/"}
+  return find_sound(SFX_name, dirs_to_check)
+end
+
+--returns audio source based on character and music_type (normal, danger, normal_start, or danger_start)
+function find_music(character, music_type)
+  local found_file
+  local character_music_overrides_stage_music = check_supported_extensions("sounds/"..sounds_dir.."characters/"..character.."/"..normal)
+  if character_music_overrides_stage_music then
+    found_file = check_supported_extensions("sounds"..sounds_dir.."/characters/"..character.."/"..music_type)
+    return found_file
+  elseif stages[character] then
+    found_file = check_supported_extensions("sounds"..sounds_dir.."/music/"..stages[character]..music_type)
+    return found_file
+  else
+    --nothing, I think...  --TODO: check this
+  end
+end
+
+function find_character_SFX(SFX_name)
+  local dirs_to_check
+  -- TODO: this whole function
+end
+
+function SFX_init()
+
+end
+
+--returns the full path, name, and extension of the first found file, or nil if it could not find a file
+function check_supported_extensions(path_and_filename)
+  local ret
+  for k, extension in ipairs(supported_sound_formats) do
+    if love.filesystem.isFile(path_and_filename..extension) then
+      return path_and_filename..extension
+    end
+  end
+  return nil
 end
 
 function sound_init()
@@ -34,7 +86,7 @@ function sound_init()
   SFX_GarbageThud_Play = 0
   sounds = {
     SFX = {
-      cur_move = love.audio.newSource("sounds/"..sounds_dir.."/SFX/06move.ogg", "static"),
+      cur_move = find_generic_SFX("06move.ogg", "static"),
       swap = love.audio.newSource("sounds/"..sounds_dir.."/SFX/08swap.ogg", "static"),
       land = love.audio.newSource("sounds/"..sounds_dir.."/SFX/12cLand.ogg", "static"),
       fanfare1 = love.audio.newSource("sounds/"..sounds_dir.."/SFX/F6Fanfare1.ogg", "static"),
