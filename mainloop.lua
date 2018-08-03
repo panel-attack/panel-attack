6a4c2dd174f6c1ca0547f10fda7cb942f75bdb61
jon12156 <32025765+jon12156@users.noreply.github.com>
2018-08-02 18:05:13 -0600

    ?

--- mainloop.lua
+++ mainloop.lua
@@ -5,6 +5,7 @@ local main_select_mode, main_endless, make_main_puzzle, main_net_vs_setup,
   main_config_input, main_dumb_transition, main_select_puzz,
   menu_up, menu_down, menu_left, menu_right, menu_enter, menu_escape,
   main_replay_vs, main_local_vs_setup, main_local_vs, menu_key_func,
+  multi_func, normal_key, main_set_name, main_character_select, main_net_vs_lobby,
   main_local_vs_yourself_setup, main_local_vs_yourself,
   main_options, exit_options_menu
 
