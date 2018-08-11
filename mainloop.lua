d011b16a336f90e6827a3f760167c905e8305c40
Jon Willahan <32025765+jon12156@users.noreply.github.com>
2018-08-11 00:16:55 -0600

    bugfix for crash after you went to 1p vs yourself character select after playing 2p_net_vs

--- mainloop.lua
+++ mainloop.lua
@@ -1341,6 +1341,8 @@ end
 
 function main_local_vs_yourself_setup()
   my_name = config.name or "Player 1"
+  op_name = nil
+  op_state = nil
   character_select_mode = "1p_vs_yourself"
   return main_character_select
 end
