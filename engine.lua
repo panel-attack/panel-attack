8f39f4539cc3824cb3124cab1e987e6e99ce247c
Jon Willahan <32025765+jon12156@users.noreply.github.com>
2018-11-15 00:35:34 -0700

    Telegraph initial progress
    no graphics for it yet, but it appears a concept of a telegraph, even if not displayed on screen, will help us order garbage and have it drop at the right times.

--- engine.lua
+++ engine.lua
@@ -358,6 +358,57 @@ function GarbageQueue.len(self)
   return ret
 end
 
+function GarbageQueue.grow_chain(self)
+-- TODO: this should increase the size of the first chain garbage by 1.
+-- This is used by the telegraph to increase the size of the chain garbage being built
+-- or add a 6-wide if there is not chain garbage yet in the queue
+end
+
+Telegraph = class(function(self, sender, recipient)
+  self.garbage_queue = new GarbageQueue()
+  self.stopper = { garbage_type, size, frame_to_release}
+  self.sender = sender
+  self.recipient = recipient
+end)
+
+function Telegraph.push(self, attack_type, attack_size)
+  self.stopper = {garbage_type=attack_type, attack_size, frame_to_release=self.stack.CLOCK+GARBAGE_TRANSIT_TIME+GARBAGE_DELAY}
+  if attack_type == "chain" then
+    self.garbage_queue:grow_chain()
+  elseif attack_type == "combo" then
+    local garbage = {--[[TODO: pull this code from sharpobject's existing code for changing combos to garbage]]}
+    self.garbage_queue:push(garbage)
+  end
+end
+
+function Telegraph.pop_all_ready_garbage()
+  local ready_garbage = {}
+  if self.stopper and self.stopper.frame_to_release <= self.recipient.CLOCK then
+    self.stopper = nil
+  end
+  if not self.stopper then
+    local next_block = {}
+    local number_of_blocks = self.garbage_queue:len()
+    for i=1, number_of_blocks do 
+      ready_garbage[i] = self.garbage_queue:pop()
+    end
+    return ready_garbage
+  elseif self.stopper and self.stopper.garbage_type == "chain" then
+    return {} --waiting on sender chain to end
+  elseif self.stopper and self.stopper.garbage_type == "combo" and stopper.garbage then
+    local next_block_type = "combo"
+    local next_in_queue = self.garbage_queue:peek()
+    while not next_in_queue[4]--[[is_from_chain]] and next_in_queue[1]--[[width]] < self.stopper.size do
+      ready_garbage[#ready_garbage+1] = self.garbage_queue:pop()
+      next_in_queue = self.garbage_queue:peek()
+    end
+    return ready_garbage
+  end
+end
+function Telegraph.sender_chain_ended()
+  self.stopper = nil
+end
+
 do
   local exclude_hover_set = {matched=true, popping=true, popped=true,
       hovering=true, falling=true}
