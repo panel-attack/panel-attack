1834d48588ff8bd651ac07efe94050f9ab572173
jon12156 <32025765+jon12156@users.noreply.github.com>
2019-01-26 10:51:48 -0700

    bump to version 034 (beta)

--- server.lua
+++ server.lua
@@ -27,7 +27,7 @@ local NAME_LENGTH_LIMIT = 16
 local sep = package.config:sub(1, 1) --determines os directory separator (i.e. "/" or "\")
 
 
-local VERSION = "033"
+local VERSION = "034"
 local type_to_length = {H=4, E=4, F=4, P=8, I=2, L=2, Q=8, U=2}
 local INDEX = 1
 local connections = {}
