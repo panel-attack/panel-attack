47574d15c948bb043be9b14d16d0def3ada91fcb
jon12156 <32025765+jon12156@users.noreply.github.com>
2019-05-19 22:01:35 -0600

    update to love 11.3
    I am now releasing with a nightly build of beta Love 11.3 from here: https://ci.appveyor.com/api/buildjobs/83j1eks2ar13t3d9/artifacts/build%2Flove-11.3-win32.zip
    
    This resolves a major graphics performance issue for Intel (and Adreno) Graphics

--- conf.lua
+++ conf.lua
@@ -12,7 +12,7 @@ function love.conf(t)
   t.modules.sound = true
   t.modules.physics = false
   t.identity = "Panel Attack"
-  t.version = "11.2"
+  t.version = "11.3"
   t.release = false
   t.externalstorage = true
 
