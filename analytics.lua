6a22373c09fa2fdcad0188195620b1151874e5cf
Éole <filo.mystere@gmail.com>
2019-10-18 12:43:31 +0200

    Fix crash upon loading with a missing analytics.json file

--- analytics.lua
+++ analytics.lua
@@ -25,7 +25,7 @@ analytics = {
                }
            }
 
-function analytics_init()
+function analytics_init() pcall(function()
   if not config.enable_analytics then
     return
   end
@@ -42,9 +42,9 @@ function analytics_init()
 
   analytics.version = analytics_version
   file:close()
-end
+end) end
 
-local function output_pretty_analytics()
+local function output_pretty_analytics() pcall(function()
   if not config.enable_analytics then
     return
   end
@@ -74,9 +74,9 @@ local function output_pretty_analytics()
   file:open("w")
   file:write(text)
   file:close()
-end
+end) end
 
-function write_analytics_files()
+function write_analytics_files() pcall(function()
   if not config.enable_analytics then
     return
   end
@@ -87,7 +87,7 @@ function write_analytics_files()
   file:close()
 
   output_pretty_analytics()
-end
+end) end
 
 function analytics_register_destroyed_panels(amount)
   if not config.enable_analytics then
