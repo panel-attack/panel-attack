0aee261083044b88b917fd7cbf85d3f7eff3047a
jon12156 <32025765+jon12156@users.noreply.github.com>
2020-01-01 21:15:17 -0700

    update rating thresholds
    These will probably be changed again soon.

--- server_globals.lua
+++ server_globals.lua
@@ -6,12 +6,12 @@ panel_color_to_number = { ["A"]=1, ["B"]=2, ["C"]=3, ["D"]=4, ["E"]=5, ["F"]=6, 
                           ["0"]=0}
 leagues = { {league="Newcomer",     min_rating = -1000},
             {league="Bronze",       min_rating = 1},
-            {league="Silver",       min_rating = 1300},
-            {league="Gold",         min_rating = 1450},
-            {league="Platinum",     min_rating = 1650},
-            {league="Diamond",      min_rating = 1900},
-            {league="Master",       min_rating = 2250},
-            {league="Grandmaster",  min_rating = 2350}
+            {league="Silver",       min_rating = 1176},
+            {league="Gold",         min_rating = 1376},
+            {league="Platinum",     min_rating = 1519},
+            {league="Diamond",      min_rating = 1670},
+            {league="Master",       min_rating = 1926},
+            {league="Grandmaster",  min_rating = 2139}
           }
 --[[leagues = { {league="Newcomer",     min_rating = -1000},
             {league="Bronze",       min_rating = 1},
