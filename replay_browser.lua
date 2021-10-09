1838bdf7fbd7071a7e075049f41f0f760245edd7
Shosoul <18148657+Shosoul@users.noreply.github.com>
2021-10-08 22:20:31 -0400

    Missing folder retreat option
    If the replay browser opens to a non-existant folder, users can now just simply select "<up>" to go back in directories until they find an existing directory.

--- replay_browser.lua
+++ replay_browser.lua
@@ -216,9 +216,8 @@ function replay_browser.main()
 end
 
 function set_replay_browser_path(path)
-  local newpath = string.sub(path, (string.len(replay_browser.base_path) + 1))
-  print(newpath)
-  replay_browser.current_path = newpath
+  local new_path = string.sub(path, (string.len(replay_browser.base_path) + 1)).."/"
+  replay_browser.current_path = new_path
 end
 
 return replay_browser
