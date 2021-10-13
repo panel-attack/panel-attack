7adca805e53bd2aced6425850821355e5711e266
Shosoul <18148657+Shosoul@users.noreply.github.com>
2021-10-13 01:05:29 -0400

    Revert "Update util.lua"
    This reverts commit 148452f2f2bb09916011885968d8fc78202ad709.

--- util.lua
+++ util.lua
@@ -279,16 +279,3 @@ function uncompress_input_string(compressedStr)
     return ucStr
   end
 end
-
-function dump(o)
-    if type(o) == 'table' then
-       local s = '{ '
-       for k,v in pairs(o) do
-          if type(k) ~= 'number' then k = '"'..k..'"' end
-          s = s .. '['..k..'] = ' .. dump(v) .. ','
-       end
-       return s .. '} '
-    else
-       return tostring(o)
-    end
-  end
