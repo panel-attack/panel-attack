efef5e384b420c978bab551afad78d6abb4df7c8
Shosoul <18148657+Shosoul@users.noreply.github.com>
2021-11-26 20:22:00 -0500

    Love File Powershell Script
    This Powershell script automatically creates a love file that we can upload to the server right away when we want to update the game.

---
+++ CreateAlphaLoveFile.ps1
@@ -0,0 +1,10 @@
+﻿$Time = Get-Date 
+$UTCTime = $Time.ToUniversalTime().ToString("yyyy-MM-dd_HH-mm-ss")
+$FileNameZip = "panel-alpha-$($UTCTime).zip"
+
+$compress = @{
+Path = ".\*"
+DestinationPath = ".\$($FileNameZip)"
+}
+
+Compress-Archive @compress; Rename-Item -Path ".\$($FileNameZip)" -NewName $($FileNameZip).Replace(".zip", ".love")
