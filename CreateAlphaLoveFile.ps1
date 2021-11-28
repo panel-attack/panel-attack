9f4dc40e56289d8b0f8fa93859df2fea1c167b7e
Shosoul <18148657+Shosoul@users.noreply.github.com>
2021-11-28 14:05:34 -0500

    Update CreateAlphaLoveFile.ps1
    Added a check to make sure the file was in the right spot in addition to proper spacing

--- CreateAlphaLoveFile.ps1
+++ CreateAlphaLoveFile.ps1
@@ -1,12 +1,18 @@
 ﻿#This file may need to be unblocked on your computer in order to run. Go to your file properties to do this.
 
-$Time = Get-Date 
-$UTCTime = $Time.ToUniversalTime().ToString("yyyy-MM-dd_HH-mm-ss")
-$FileNameZip = "panel-alpha-$($UTCTime).zip"
 
-$compress = @{
-Path = "$($PSScriptRoot)\*"
-DestinationPath = "$($PSScriptRoot)\$($FileNameZip)"
+
+if (Test-Path "$($PSScriptRoot)\main.lua") {
+    $Time = Get-Date 
+    $UTCTime = $Time.ToUniversalTime().ToString("yyyy-MM-dd_HH-mm-ss")
+    $FileNameZip = "panel-alpha-$($UTCTime).zip"
+    $compress = @{
+        Path            = "$($PSScriptRoot)\*"
+        DestinationPath = "$($PSScriptRoot)\$($FileNameZip)"
+    }
+      
+    Compress-Archive @compress; Rename-Item -Path "$($PSScriptRoot)\$($FileNameZip)" -NewName $($FileNameZip).Replace(".zip", ".love")
 }
-
-Compress-Archive @compress; Rename-Item -Path "$($PSScriptRoot)\$($FileNameZip)" -NewName $($FileNameZip).Replace(".zip", ".love")
+else {
+    Write-Error -Message "main.lua was not found in $($PSScriptRoot)"
+}
