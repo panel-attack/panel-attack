86df881e2cf9f8ebc71c22441fb4f48088cb2a90
Shosoul <18148657+Shosoul@users.noreply.github.com>
2021-11-28 13:33:03 -0500

    Create CreateAlphaLoveFile.ps1
    Creates file for uploading to the server for the alpha build.

---
+++ CreateAlphaLoveFile.ps1
@@ -0,0 +1,12 @@
+﻿#This file may need to be unblocked on your computer in order to run. Go to your file properties to do this.
+
+$Time = Get-Date 
+$UTCTime = $Time.ToUniversalTime().ToString("yyyy-MM-dd_HH-mm-ss")
+$FileNameZip = "panel-alpha-$($UTCTime).zip"
+
+$compress = @{
+Path = ".\*"
+DestinationPath = ".\$($FileNameZip)"
+}
+
+Compress-Archive @compress; Rename-Item -Path ".\$($FileNameZip)" -NewName $($FileNameZip).Replace(".zip", ".love")
