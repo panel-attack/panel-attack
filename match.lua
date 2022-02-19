afd50b35b0f26bbf4dd01a8bf0156b3c9338636f
Shosoul <18148657+Shosoul@users.noreply.github.com>
2022-02-18 22:27:03 -0500

    Prevent Crash

--- match.lua
+++ match.lua
@@ -14,7 +14,7 @@ Match =
     self.attackEngine = nil
     self.current_music_is_casual = true
     self.startTimestamp = os.time(os.date("*t"))
-    if P2 or mode == "vs" and GAME.battleRoom then
+    if (P2 or mode == "vs") and GAME.battleRoom then
       GAME.rich_presence:setPresence(
       (GAME.battleRoom.spectating and "Spectating" or "Playing") .. " a " .. match_type .. " match",
       GAME.battleRoom.playerNames[1] .. " vs " .. (GAME.battleRoom.playerNames[2] or "themselves"),
