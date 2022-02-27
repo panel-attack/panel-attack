663333f2d24cd05c1ba3a74328c3bc5080880744
Jean-Denis Boivin <jd@boiv.in>
2022-02-27 00:16:52 -0500

    Fix inaccurate `panels_to_speedup` (#188)
    In endless and time attack we are not crediting the last panel that pops towards increasing the speed. That means time attack and endless are a little bit easier than they should be.

--- engine.lua
+++ engine.lua
@@ -1363,7 +1363,10 @@ function Stack.simulate(self)
                 end
                 self.poppedPanelIndex = panel.combo_index
                 panel.color = 0
-                if (panel.chaining) then
+		if self.panels_to_speedup then
+	          self.panels_to_speedup = self.panels_to_speedup - 1
+	        end
+                if panel.chaining then
                   self.n_chain_panels = self.n_chain_panels - 1
                 end
                 panel:clear_flags()
