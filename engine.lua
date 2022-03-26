d09a85417ab12719979747377a24147bfe8b6c53
Shosoul <18148657+Shosoul@users.noreply.github.com>
2022-03-26 17:31:03 -0400

    Revert "Testing alternate rollback (#480)" (#482)
    This reverts commit dec801dc4f3ebac846081ca301fe89b94be735cf.

--- engine.lua
+++ engine.lua
@@ -402,8 +402,8 @@ end
 function Stack.restoreFromRollbackCopy(self, other)
   self:rollbackCopy(other, self)
   if self.telegraph then
-    self.telegraph.owner = self.garbage_target
-    self.telegraph.sender = self
+    self.telegraph.owner = self
+    self.telegraph.sender = self.garbage_target
   end
   -- The remaining inputs is the confirmed inputs not processed yet for this clock time
   -- We have processed CLOCK time number of inputs when we are at CLOCK, so we only want to process the CLOCK+1 input on
@@ -427,12 +427,18 @@ function Stack.rollbackToFrame(self, frame)
     assert(prev_states[frame])
     self:restoreFromRollbackCopy(prev_states[frame])
 
-    if self.garbage_target and self.garbage_target.later_garbage then
+    if self.garbage_target and self.garbage_target.telegraph then
       -- The garbage that we send this time might (rarely) not be the same
       -- as the garbage we sent before.  Wipe out the garbage we sent before...
-      for k, v in pairs(self.garbage_target.later_garbage) do
+      for k, v in pairs(self.garbage_target.telegraph.pendingGarbage) do
         if k > frame then
-          self.garbage_target.later_garbage[k] = nil
+          self.garbage_target.telegraph.pendingGarbage[k] = nil
+        end
+      end
+
+      for k, v in pairs(self.garbage_target.telegraph.pendingChainingEnded) do
+        if k > frame then
+          self.garbage_target.telegraph.pendingChainingEnded[k] = nil
         end
       end
     end
@@ -467,9 +473,9 @@ end
 
 function Stack.set_garbage_target(self, new_target)
   self.garbage_target = new_target
-  if self.telegraph then
-    self.telegraph.owner = new_target
-    self.telegraph:updatePosition()
+  if new_target.telegraph then
+    new_target.telegraph.sender = self
+    new_target.telegraph.garbage_queue.sender = self
   end
 end
 
@@ -1117,6 +1123,10 @@ function Stack.simulate(self)
       end
     end
 
+    if self.telegraph then
+      self.telegraph:update()
+    end
+
     -- Phase 2. /////////////////////////////////////////////////////////////
     -- Timer-expiring actions + falling
     local propogate_fall = {false, false, false, false, false, false}
@@ -1492,7 +1502,7 @@ function Stack.simulate(self)
       self.chain_counter = 0
 
       if self.garbage_target and self.garbage_target.telegraph then
-        self.telegraph:chainingEnded(self.CLOCK)
+        self.garbage_target.telegraph:chainingEnded(self.CLOCK)
       end
     end
 
@@ -1515,7 +1525,11 @@ function Stack.simulate(self)
     if self.telegraph then
       local to_send = self.telegraph:pop_all_ready_garbage(self.CLOCK)
       if to_send and to_send[1] then
-        self.garbage_target:receiveGarbage(self.CLOCK + GARBAGE_ATTACK_DELAY, to_send)
+        local garbage = self.later_garbage[self.CLOCK+1] or {}
+        for i = 1, #to_send do
+          garbage[#garbage + 1] = to_send[i]
+        end
+        self.later_garbage[self.CLOCK+1] = garbage
       end
     end
     
@@ -1794,20 +1808,6 @@ function Stack.simulate(self)
 
   self:update_popfxs()
   self:update_cards()
-end
-
-function Stack:receiveGarbage(frameToReceive, garbageList)
-
-  -- If we are past the frame the attack would be processed we need to rollback
-  if self.CLOCK > frameToReceive then
-    self:rollbackToFrame(frameToReceive)
-  end
-
-  local garbage = self.later_garbage[frameToReceive] or {}
-  for i = 1, #garbageList do
-    garbage[#garbage + 1] = garbageList[i]
-  end
-  self.later_garbage[frameToReceive] = garbage
 end
 
 function Stack:updateFramesBehind()
@@ -2330,8 +2330,8 @@ function Stack.check_matches(self)
 
   if (combo_size ~= 0) then
     self.combos[self.CLOCK] = combo_size
-    if self.garbage_target and self.telegraph and metal_count == 3 and combo_size >= 3 then
-      self.telegraph:push({6, 1, true, false}, first_panel_col, first_panel_row, self.CLOCK)
+    if self.garbage_target and self.garbage_target.telegraph and metal_count == 3 and combo_size >= 3 then
+      self.garbage_target.telegraph:push({6, 1, true, false}, first_panel_col, first_panel_row, self.CLOCK)
     end
     self.analytic:register_destroyed_panels(combo_size)
     if (combo_size > 3) then
@@ -2349,16 +2349,16 @@ function Stack.check_matches(self)
       end
 
       self:enqueue_card(false, first_panel_col, first_panel_row, combo_size)
-      if self.garbage_target and self.telegraph then
+      if self.garbage_target and self.garbage_target.telegraph then
         if metal_count > 3 then
           for i = 3, metal_count do
-            self.telegraph:push({6, 1, true, false}, first_panel_col, first_panel_row, self.CLOCK)
+            self.garbage_target.telegraph:push({6, 1, true, false}, first_panel_col, first_panel_row, self.CLOCK)
           end
         end
         if metal_count ~= combo_size then
           local combo_pieces = combo_garbage[combo_size]
           for i=1,#combo_pieces do
-            self.telegraph:push({combo_pieces[i], 1, false, false}, first_panel_col, first_panel_row, self.CLOCK)
+            self.garbage_target.telegraph:push({combo_pieces[i], 1, false, false}, first_panel_col, first_panel_row, self.CLOCK)
           end
         end
       end
@@ -2376,8 +2376,8 @@ function Stack.check_matches(self)
       self:enqueue_card(true, first_panel_col, first_panel_row, self.chain_counter)
     --EnqueueConfetti(first_panel_col<<4+P1StackPosX+4,
     --          first_panel_row<<4+P1StackPosY+self.displacement-9);
-      if self.garbage_target and self.telegraph then
-        self.telegraph:push({6, self.chain_counter - 1, false, true}, first_panel_col, first_panel_row, self.CLOCK)
+      if self.garbage_target and self.garbage_target.telegraph then
+        self.garbage_target.telegraph:push({6, self.chain_counter - 1, false, true}, first_panel_col, first_panel_row, self.CLOCK)
       end
     end
     local chain_bonus = self.chain_counter
