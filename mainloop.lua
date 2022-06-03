93266ae975fba8c65468378fedec1d58c74aa865
Shosoul <18148657+Shosoul@users.noreply.github.com>
2022-06-03 00:02:52 -0400

    Fix bug where basic training modes crash (#540)
    * Fix bug where basic training modes crash
    
    * Fix chain handling
    
    * Revert "Fix chain handling"
    
    This reverts commit 22beaa4d399f7613dc85a81cbb7d8668b30a166d.

--- mainloop.lua
+++ mainloop.lua
@@ -565,17 +565,23 @@ local function main_endless_time_setup(mode, speed, difficulty, level)
 
 end
 
-local function createBasicTrainingMode(name, width, height) 
+local function createBasicTrainingMode(name, width, height, illegalGarbage) 
 
   local delayBeforeStart = 150
-  local delayBeforeRepeat = 91
+  local delayBeforeRepeat = 900
+  local attacksPerVolley = 50
   local attackPatterns = {}
 
-  if height > 1 and width == 6 then -- chain
-    attackPatterns[#attackPatterns+1] = {startTime = 1, height = height, chain = 1, chainEndDelta = GARBAGE_TRANSIT_TIME}
+  if height > 1 and width == 6 and not illegalGarbage then -- chain
+    local chainEndTime = height + 1 + GARBAGE_TRANSIT_TIME
+    
+    for i = 1, height + 1 do
+      local startTime = math.floor(i / (height + 1) * chainEndTime)
+      attackPatterns[#attackPatterns+1] = {width = width, height = 1, startTime = startTime, metal = false, chain = true, endsChain = false}
+    end
+
+    attackPatterns[#attackPatterns+1] = {width = width, height = 1, startTime = chainEndTime, metal = false, chain = true, endsChain = true}
   else -- combo (or illegal garbage)
-    local attacksPerVolley = 50
-    delayBeforeRepeat = 900
     for i = 1, attacksPerVolley do
       attackPatterns[#attackPatterns+1] = {width = width, height = height, startTime = i, metal = false, chain = false, endsChain = false}
     end
@@ -593,8 +599,8 @@ function training_setup()
   local customTrainingModes = {}
   customTrainingModes[0] = {name = "None"}
   customTrainingModes[1] = createBasicTrainingMode(loc("combo_storm"), 4, 1)
-  customTrainingModes[2] = createBasicTrainingMode(loc("factory"), 6, 2)
-  customTrainingModes[3] = createBasicTrainingMode(loc("large_garbage"), 6, 12)
+  customTrainingModes[2] = createBasicTrainingMode(loc("factory"), 6, 2, true)
+  customTrainingModes[3] = createBasicTrainingMode(loc("large_garbage"), 6, 12, true)
   for customfile, value in ipairs(trainings) do
     customTrainingModes[#customTrainingModes+1] = value
   end
@@ -660,7 +666,7 @@ function training_setup()
   end
 
   local function start_custom_game()
-    customTrainingModes[0] = createBasicTrainingMode("", trainingModeSettings.width, trainingModeSettings.height)
+    customTrainingModes[0] = createBasicTrainingMode("", trainingModeSettings.width, trainingModeSettings.height, true)
     ret = {main_local_vs_yourself_setup, {customTrainingModes[customModeID]}}
   end
 
