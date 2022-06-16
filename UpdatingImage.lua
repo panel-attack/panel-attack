194ce74929eae16ad1cf0945349e436456eebdbc
JamBox <8935453+JamesVanBoxtel@users.noreply.github.com>
2022-06-15 18:38:25 -0700

    Fixes

--- UpdatingImage.lua
+++ UpdatingImage.lua
@@ -15,19 +15,24 @@ UpdatingImage =
     self.width = width or self.image:getWidth()
     self.height = height or self.image:getHeight()
     self.totalTime = 0
+    self.quad = nil
+    if self.tiled then
+      -- note how the Quad's width and height are larger than the image width and height.
+      self.quad = love.graphics.newQuad(0, 0, self.width, self.height, self.image:getDimensions())
+    end
   end
 )
 
 function UpdatingImage:update(dt)
   self.totalTime = self.totalTime + dt
+  if self.tiled then
+    local x = math.floor(self.totalTime * self.speedX)
+    local y = math.floor(self.totalTime * -self.speedY)
+    self.quad:setViewport(x, y, self.width, self.height, self.image:getDimensions())
+  end
 end
 
 function UpdatingImage:draw()
-  local x = math.floor(self.totalTime * self.speedX)
-  local y = math.floor(self.totalTime * -self.speedY)
-
-  -- note how the Quad's width and height are larger than the image width and height.
-  local bg_quad = love.graphics.newQuad(x, y, self.width, self.height, self.image:getDimensions())
 
   local x_scale = 1
   local y_scale = 1
@@ -36,6 +41,6 @@ function UpdatingImage:draw()
     y_scale = self.height / self.image:getHeight()
     gfx_q:push({love.graphics.draw, {self.image, 0, 0, 0, x_scale, y_scale}})
   else
-    gfx_q:push({love.graphics.draw, {self.image, bg_quad, 0, 0, 0, x_scale, y_scale}})
+    gfx_q:push({love.graphics.draw, {self.image, self.quad, 0, 0, 0, x_scale, y_scale}})
   end
 end
