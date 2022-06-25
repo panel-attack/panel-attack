788458ef3390bff270de017f025627934da33e06
KattKit <47788030+KattKit@users.noreply.github.com>
2022-06-25 00:21:50 -0400

    rewrote leaderboard loop using ipairs and set rank by username comparison (#560)
    * rewrote leaderboard loop using ipairs and set rank by username comparison
    
    * Apply suggestions from code review
    
    Co-authored-by: Kathrine Hewlett <KattKit@noreply.github.com>
    Co-authored-by: JamBox <8935453+JamesVanBoxtel@users.noreply.github.com>

--- mainloop.lua
+++ mainloop.lua
@@ -1057,10 +1057,10 @@ function main_net_vs_lobby()
           lobby_menu:show_controls(true)
         end
         leaderboard_report = msg.leaderboard_report
-        for i = #leaderboard_report, 1, -1 do
-          local v = leaderboard_report[i]
-          if v.is_you then
-            my_rank = k
+        for rank = #leaderboard_report, 1, -1 do
+          local user = leaderboard_report[rank]
+          if user.user_name == config.name then
+            my_rank = rank
           end
         end
         leaderboard_first_idx_to_show = math.max((my_rank or 1) - 8, 1)
