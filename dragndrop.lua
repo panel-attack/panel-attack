f6367547a6ab1f05cbb1ddc75b7b9acae8b621e0
Endaris <Endaris@users.noreply.github.com>
2022-07-20 15:14:24 -0500

    restrict import to main menu

--- dragndrop.lua
+++ dragndrop.lua
@@ -1,64 +1,67 @@
 require("util")
 
 function love.directorydropped(path)
-  local getDirItems = love.filesystem.getDirectoryItems
-  love.filesystem.mount(path, "droppedFolder")
-  local pathSegments = love.system.getOS() == "Windows" and split(path, "\\") or split(path, "/")
-  local dirName = pathSegments[#pathSegments]
-  local droppedFiles = getDirItems("droppedFolder")
-  local validFolderNames = { "characters", "panels", "puzzles", "stages", "themes" }
-  local existingAssets = {}
-  local imported = {}
-  local invalid = {}
-  local unknown = {}
+  if CLICK_MENUS[#CLICK_MENUS] and #CLICK_MENUS[#CLICK_MENUS].buttons > 0 and 
+  CLICK_MENUS[#CLICK_MENUS].buttons[#CLICK_MENUS[#CLICK_MENUS].buttons].stringText == loc("mm_quit") then
+    local getDirItems = love.filesystem.getDirectoryItems
+    love.filesystem.mount(path, "droppedFolder")
+    local pathSegments = love.system.getOS() == "Windows" and split(path, "\\") or split(path, "/")
+    local dirName = pathSegments[#pathSegments]
+    local droppedFiles = getDirItems("droppedFolder")
+    local validFolderNames = { "characters", "panels", "puzzles", "stages", "themes" }
+    local existingAssets = {}
+    local imported = {}
+    local invalid = {}
+    local unknown = {}
 
-  local function validateMod(type, folderName)
-    return true
-  end
+    local function validateMod(type, folderName)
+      return true
+    end
 
-  local function importMods(assetType)
-    local files = getDirItems("droppedFolder/"..assetType)
-    local paFiles = getDirItems(assetType)
-    for j = 1, #files do
-      local dir = assetType.."/"..files[j]
-      local file = "droppedFolder/"..dir
-      if validateMod(assetType, file) then
-        -- does the mod already exist?
-        if not table.contains(paFiles, file) then
-          -- no, let's go
-          recursive_copy(file, dir)
-          imported[#imported+1] = dir
+    local function importMods(assetType)
+      local files = getDirItems("droppedFolder/"..assetType)
+      local paFiles = getDirItems(assetType)
+      for j = 1, #files do
+        local dir = assetType.."/"..files[j]
+        local file = "droppedFolder/"..dir
+        if validateMod(assetType, file) then
+          -- does the mod already exist?
+          if not table.contains(paFiles, file) then
+            -- no, let's go
+            recursive_copy(file, dir)
+            imported[#imported+1] = dir
+          else
+            -- save mod name to prompt user for decision later
+            existingAssets[#existingAssets+1] = { source = file, assetType = assetType, destination = dir }
+          end
         else
-          -- save mod name to prompt user for decision later
-          existingAssets[#existingAssets+1] = { source = file, assetType = assetType, destination = dir }
+          invalid[#invalid+1] = file
         end
-      else
-        invalid[#invalid+1] = file
       end
     end
-  end
 
-  local function promptOverwrite(assetName)
-    return false
-  end
-
-  for i = 1, #droppedFiles do
-    local assetType = droppedFiles[i]
-    if love.filesystem.getInfo(assetType, "directory")
-        and table.contains(validFolderNames, assetType) then
-      importMods(assetType)
-    else
-      unknown[#unknown+1] = dirName
+    local function promptOverwrite(assetName)
+      return false
     end
-  end
 
-  for i = 1, #existingAssets do
-    -- prompt
-    if promptOverwrite(existingAssets[i]) then
-      recursive_copy(existingAssets[i].source, existingAssets[i].destination)
+    for i = 1, #droppedFiles do
+      local assetType = droppedFiles[i]
+      if love.filesystem.getInfo(assetType, "directory")
+          and table.contains(validFolderNames, assetType) then
+        importMods(assetType)
+      else
+        unknown[#unknown+1] = dirName
+      end
     end
-  end
 
-  -- report about unknown files and invalid mods
-  love.filesystem.unmount("droppedFolder")
+    for i = 1, #existingAssets do
+      -- prompt
+      if promptOverwrite(existingAssets[i]) then
+        recursive_copy(existingAssets[i].source, existingAssets[i].destination)
+      end
+    end
+
+    -- report about unknown files and invalid mods
+    love.filesystem.unmount("droppedFolder")
+  end
 end
