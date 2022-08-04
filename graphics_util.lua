48937a47350f872f361b6609429287f77044270c
Shosoul <18148657+Shosoul@users.noreply.github.com>
2022-08-03 18:42:14 -0700

    Fix filter error (#641)

--- graphics_util.lua
+++ graphics_util.lua
@@ -41,7 +41,7 @@ function GraphicsUtil.privateLoadImageWithExtensionAndScale(pathAndName, extensi
       -- We would like to use linear for shrinking and nearest for growing,
       -- but there is a bug in some drivers that doesn't allow for min and mag to be different
       -- to work around this, calculate if we are shrinking or growing and use the right filter on both.
-      if GAME.canvasXScale > scale then
+      if GAME.canvasXScale >= scale then
         result:setFilter("nearest", "nearest")
       else
         result:setFilter("linear", "linear")
