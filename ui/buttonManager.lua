2845db5edb960c9088b1a7e2785fb29ff63d9f8c
Sam Ekong <samekong@gmail.com>
2022-08-21 17:48:04 -0700

    Fixed LevelSlider and added isEnabled

--- ui/buttonManager.lua
+++ ui/buttonManager.lua
@@ -1,42 +1,1 @@
---@module button_manager
-local buttonManager = {
-  buttons = {}
-}
-
-local selectedButton = nil
-
-function buttonManager.update()
-  if selectedButton and love.mouse.isDown(1) then
-    selectedButton.onMousePressed()
-  end
-end
-
-function buttonManager.draw()
-  for id, button in pairs(buttonManager.buttons) do
-    button:draw()
-  end
-end
-
-function buttonManager.mousePressed(x, y)
-  local canvasX, canvasY = GAME:transform_coordinates(x, y)
-  for id, button in pairs(buttonManager.buttons) do
-    if button:isSelected(canvasX, canvasY) then
-      button.onMouseDown()
-      selectedButton = button
-      break
-    end
-  end
-end
-
-function buttonManager.mouseReleased(x, y)
-  local canvasX, canvasY = GAME:transform_coordinates(x, y)
-  if selectedButton then
-    selectedButton.onMouseUp()
-    if selectedButton:isSelected(canvasX, canvasY) then
-      selectedButton.onClick()
-    end
-    selectedButton = nil
-  end
-end
-
-return buttonManager
+--@module button_manager local buttonManager = {   buttons = {} }  local selectedButton = nil  function buttonManager.update()   if selectedButton and love.mouse.isDown(1) then     selectedButton.onMousePressed()   end end  function buttonManager.draw()   for id, button in pairs(buttonManager.buttons) do     button:draw()   end end  function buttonManager.mousePressed(x, y)   local canvasX, canvasY = GAME:transform_coordinates(x, y)   for id, button in pairs(buttonManager.buttons) do     if button.isEnabled and button:isSelected(canvasX, canvasY) then       button.onMouseDown()       selectedButton = button       break     end   end end  function buttonManager.mouseReleased(x, y)   local canvasX, canvasY = GAME:transform_coordinates(x, y)   if selectedButton and selectedButton.isEnabled then     selectedButton.onMouseUp()     if selectedButton:isSelected(canvasX, canvasY) then       selectedButton.onClick()     end     selectedButton = nil   end end  return buttonManager
--- ui/inputFieldManager.lua
+++ ui/inputFieldManager.lua
@@ -1,54 +1,1 @@
-local input = require("inputManager")
-
---@module inputFieldManager
-local inputFieldManager = {
-  inputFields = {}
-}
-
-local selectedInputField = nil
-
-function inputFieldManager.update()
-  if input:isPressedWithRepeat("backspace", .25, .1) and selectedInputField then
-    selectedInputField:onBackspace()
-  end
-  
-  if input:isPressedWithRepeat("left", .25, .1) and selectedInputField then
-    selectedInputField:onMoveCursor(-1)
-  end
-  
-  if input:isPressedWithRepeat("right", .25, .1) and selectedInputField then
-    selectedInputField:onMoveCursor(1)
-  end
-end
-
-function inputFieldManager.draw()
-  for id, inputField in pairs(inputFieldManager.inputFields) do
-    inputField:draw()
-  end
-end
-
-function inputFieldManager.mousePressed(x, y)
-  local canvasX, canvasY = GAME:transform_coordinates(x, y)
-  if selectedInputField then
-    selectedInputField.hasFocus = false
-  end
-  selectedInputField = nil
-  love.keyboard.setTextInput(false)
-
-  for id, inputField in pairs(inputFieldManager.inputFields) do
-    if inputField:isSelected(canvasX, canvasY) then
-      inputField:setFocus(x, y)
-      love.keyboard.setTextInput(true)
-      selectedInputField = inputField
-      break
-    end
-  end
-end
-
-function inputFieldManager.textInput(t)
-  if selectedInputField then
-    selectedInputField:textInput(t)
-  end
-end
-
-return inputFieldManager
+local input = require("inputManager")  --@module inputFieldManager local inputFieldManager = {   inputFields = {} }  local selectedInputField = nil  function inputFieldManager.update()   if not selectedInputField or not selectedInputField.isEnabled then     return   end    if input:isPressedWithRepeat("backspace", .25, .1)then     selectedInputField:onBackspace()   end      if input:isPressedWithRepeat("left", .25, .1) then     selectedInputField:onMoveCursor(-1)   end      if input:isPressedWithRepeat("right", .25, .1) then     selectedInputField:onMoveCursor(1)   end end  function inputFieldManager.draw()   for id, inputField in pairs(inputFieldManager.inputFields) do     inputField:draw()   end end  function inputFieldManager.mousePressed(x, y)   local canvasX, canvasY = GAME:transform_coordinates(x, y)   if selectedInputField then     selectedInputField.hasFocus = false   end   selectedInputField = nil   love.keyboard.setTextInput(false)    for id, inputField in pairs(inputFieldManager.inputFields) do     if inputField.isEnabled and inputField:isSelected(canvasX, canvasY) then       inputField:setFocus(x, y)       love.keyboard.setTextInput(true)       selectedInputField = inputField       break     end   end end  function inputFieldManager.textInput(t)   if selectedInputField and selectedInputField.isEnabled then     selectedInputField:textInput(t)   end end  return inputFieldManager
