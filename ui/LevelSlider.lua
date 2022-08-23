942bcd1da22af9e1a5d585b8ef61f8fe36443067
Sam Ekong <samekong@gmail.com>
2022-08-23 01:16:59 -0700

    Fixed line endings

--- ui/LevelSlider.lua
+++ ui/LevelSlider.lua
@@ -1,1 +1,33 @@
-local class = require("class") local Slider = require("ui.Slider") local util = require("util")  --@module LevelSlider local LevelSlider = class(   function(self, options)     self.min = 1     self.max = #themes[config.theme].images.IMG_levels     self.value = options.value or math.floor((self.max - self.min) / 2)     self.tickLength = 11   end,   Slider)  function LevelSlider:isSelected(x, y)   local screenX, screenY = self:getScreenPos()   return x >= screenX and x <= screenX + (self.max - self.min + 1) * self.tickLength and y >= screenY and y <= screenY + self.tickLength * 2 end  function LevelSlider:draw()   local screenX, screenY = self:getScreenPos()   for i, level_img in ipairs(themes[config.theme].images.IMG_levels) do     local img = i <= self.value and        themes[config.theme].images.IMG_levels[i] or        themes[config.theme].images.IMG_levels_unfocus[i]     local width = img:getWidth()     GAME.gfx_q:push({love.graphics.draw, {img, screenX + (i - 1) * self.tickLength, screenY, 0, self.tickLength / img:getWidth(), self.tickLength / img:getHeight(), 0, 0}})   end   local cursor_image = themes[config.theme].images.IMG_level_cursor   GAME.gfx_q:push({love.graphics.draw, {cursor_image, screenX + (self.value - 1 + .5) * self.tickLength, screenY + self.tickLength, 0, 1, 1, cursor_image:getWidth() / 2, 0}}) end  return LevelSlider
+local class = require("class")
+local Slider = require("ui.Slider")
+local util = require("util")
+
+--@module LevelSlider
+local LevelSlider = class(
+  function(self, options)
+    self.min = 1
+    self.max = #themes[config.theme].images.IMG_levels
+    self.value = options.value or math.floor((self.max - self.min) / 2)
+    self.tickLength = 11
+  end,
+  Slider)
+
+function LevelSlider:isSelected(x, y)
+  local screenX, screenY = self:getScreenPos()
+  return x >= screenX and x <= screenX + (self.max - self.min + 1) * self.tickLength and y >= screenY and y <= screenY + self.tickLength * 2
+end
+
+function LevelSlider:draw()
+  local screenX, screenY = self:getScreenPos()
+  for i, level_img in ipairs(themes[config.theme].images.IMG_levels) do
+    local img = i <= self.value and 
+      themes[config.theme].images.IMG_levels[i] or 
+      themes[config.theme].images.IMG_levels_unfocus[i]
+    local width = img:getWidth()
+    GAME.gfx_q:push({love.graphics.draw, {img, screenX + (i - 1) * self.tickLength, screenY, 0, self.tickLength / img:getWidth(), self.tickLength / img:getHeight(), 0, 0}})
+  end
+  local cursor_image = themes[config.theme].images.IMG_level_cursor
+  GAME.gfx_q:push({love.graphics.draw, {cursor_image, screenX + (self.value - 1 + .5) * self.tickLength, screenY + self.tickLength, 0, 1, 1, cursor_image:getWidth() / 2, 0}})
+end
+
+return LevelSlider
--- ui/sliderManager.lua
+++ ui/sliderManager.lua
@@ -1,1 +1,43 @@
---@module sliderManager local sliderManager = {   sliders = {} }  local selectedSlider = nil  function sliderManager.draw()   for id, slider in pairs(sliderManager.sliders) do     slider:draw()   end end  function sliderManager.mouseDragged(x, y)   local canvasX, canvasY = GAME:transform_coordinates(x, y)   if selectedSlider == nil or not selectedSlider.isEnabled then     return   end      selectedSlider:setValueFromPos(canvasX) end  function sliderManager.mouseReleased(x, y)   local canvasX, canvasY = GAME:transform_coordinates(x, y)   if selectedSlider == nil or not selectedSlider.isEnabled then     return   end    selectedSlider:setValueFromPos(canvasX)   selectedSlider = nil end  function sliderManager.mousePressed(x, y)   local canvasX, canvasY = GAME:transform_coordinates(x, y)   for id, slider in pairs(sliderManager.sliders) do     if slider.isEnabled and slider:isSelected(canvasX, canvasY) then       selectedSlider = slider       selectedSlider:setValueFromPos(canvasX)     end   end end  return sliderManager
+--@module sliderManager
+local sliderManager = {
+  sliders = {}
+}
+
+local selectedSlider = nil
+
+function sliderManager.draw()
+  for id, slider in pairs(sliderManager.sliders) do
+    slider:draw()
+  end
+end
+
+function sliderManager.mouseDragged(x, y)
+  local canvasX, canvasY = GAME:transform_coordinates(x, y)
+  if selectedSlider == nil or not selectedSlider.isEnabled then
+    return
+  end
+  
+  selectedSlider:setValueFromPos(canvasX)
+end
+
+function sliderManager.mouseReleased(x, y)
+  local canvasX, canvasY = GAME:transform_coordinates(x, y)
+  if selectedSlider == nil or not selectedSlider.isEnabled then
+    return
+  end
+
+  selectedSlider:setValueFromPos(canvasX)
+  selectedSlider = nil
+end
+
+function sliderManager.mousePressed(x, y)
+  local canvasX, canvasY = GAME:transform_coordinates(x, y)
+  for id, slider in pairs(sliderManager.sliders) do
+    if slider.isEnabled and slider:isSelected(canvasX, canvasY) then
+      selectedSlider = slider
+      selectedSlider:setValueFromPos(canvasX)
+    end
+  end
+end
+
+return sliderManager
