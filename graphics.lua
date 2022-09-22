e67d2578678e0ae8b75425541d64822aca236ce0
JamBox <8935453+JamesVanBoxtel@users.noreply.github.com>
2022-09-21 18:06:57 -0700

    Make the cards go transparent as they go up (#672)

--- graphics.lua
+++ graphics.lua
@@ -80,7 +80,10 @@ function Stack.draw_cards(self)
       local iconSize = 48 / GFX_SCALE
       local cardImage = themes[config.theme].images.IMG_cards[card.chain][card.n]
       local icon_width, icon_height = cardImage:getDimensions()
+      local fade = 1 - math.min(0.5 * ((card.frame-1) / 22), 0.5)
+      set_color(1, 1, 1, fade)
       draw(cardImage, draw_x, draw_y, 0, iconSize / icon_width, iconSize / icon_height)
+      set_color(1, 1, 1, 1)
     end
   end
 end
