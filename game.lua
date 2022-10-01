2da04052ebc86b9d988902cecc3fc2c237e46839
Sam Ekong <samekong@gmail.com>
2022-09-30 21:38:16 -0700

    Integrate scenes into game loop
     - Made Game manage more things (setup, update, draw, etc)

--- game.lua
+++ game.lua
@@ -1,18 +1,35 @@
-
 -- The main game object for tracking everything in Panel Attack.
 -- Not to be confused with "Match" which is the current battle / instance of the game.
-Game =
-  class(
+local consts = require("consts")
+local class = require("class")
+local logger = require("logger")
+local sound = require("sound")
+local analytics = require("analytics")
+local sceneManager = require("scenes.sceneManager")
+local scenes = nil
+
+--- @module Game
+local Game = class(
   function(self)
     self.scores = require("scores")
+    self.input = require("input")
     self.match = nil -- Match - the current match going on or nil if inbetween games
     self.battleRoom = nil -- BattleRoom - the current room being used for battles
     self.focused = true -- if the window is focused
     self.backgroundImage = nil -- the background image for the game, should always be set to something with the proper dimensions
+    self.foreground_overlay = nil
     self.droppedFrames = 0
     self.puzzleSets = {} -- all the puzzles loaded into the game
     self.gameIsPaused = false -- game can be paused while playing on local
     self.renderDuringPause = false -- if the game can render when you are paused
+    self.global_canvas = love.graphics.newCanvas(consts.CANVAS_WIDTH, consts.CANVAS_HEIGHT)
+    self.gfx_q = Queue()
+    self.game_updater = GAME_UPDATER
+    self.server_queue = ServerQueue()
+    self.main_menu_screen_pos = {consts.CANVAS_WIDTH / 2 - 108 + 50, consts.CANVAS_HEIGHT / 2 - 111}
+    self.config = config
+    self.localization = Localization()
+    self.replay = {}
     self.currently_paused_tracks = {} -- list of tracks currently paused
     self.rich_presence = nil
     self.muteSoundEffects = false
@@ -25,10 +42,220 @@ Game =
     self.needsAssetReload = false
     self.previousWindowWidth = 0
     self.previousWindowHeight = 0
+    
+    -- private members
+    self.pointer_hidden = false
+    self.last_x = 0
+    self.last_y = 0
+    self.input_delta = 0.0
+    self.mainloop = nil
+    -- coroutines
+    self.setup = coroutine.create(function() self:setupCo() end)
   end
 )
 
-function Game.clearMatch(self)
+function Game:setupCo()
+  -- loading various assets into the game
+  love.window.setPosition(config.windowX, config.windowY, config.display)
+  love.window.setFullscreen(config.fullscreen)
+  love.window.setVSync(config.vsync and 1 or 0)
+  
+  self:drawLoadingString("Loading localization...")
+  coroutine.yield()
+  Localization.init(localization)
+  
+  self:drawLoadingString(loc("ld_theme"))
+  coroutine.yield()
+  theme_init()
+  
+  -- stages and panels before characters since they are part of their loading!
+  self:drawLoadingString(loc("ld_stages"))
+  coroutine.yield()
+  stages_init()
+  
+  self:drawLoadingString(loc("ld_panels"))
+  coroutine.yield()
+  panels_init()
+  
+  self:drawLoadingString(loc("ld_characters"))
+  coroutine.yield()
+  characters_init()
+  
+  self:drawLoadingString(loc("ld_analytics"))
+  coroutine.yield()
+  analytics.init()
+  apply_config_volume()
+  -- create folders in appdata for those who don't have them already
+  love.filesystem.createDirectory("characters")
+  love.filesystem.createDirectory("panels")
+  love.filesystem.createDirectory("themes")
+  love.filesystem.createDirectory("stages")
+  love.filesystem.createDirectory("training")
+  
+  if #love.filesystem.getDirectoryItems("training") == 0 then
+    recursive_copy("default_data/training", "training")
+  end
+  read_attack_files("training")
+  
+  if love.system.getOS() ~= "OS X" then
+    recursiveRemoveFiles(".", ".DS_Store")
+  end
+
+  --check for game updates
+  if self.game_updater and self.game_updater.check_update_ingame then
+    wait_game_update = self.game_updater:async_download_latest_version()
+  end
+  
+  -- must be here until globally initiallized structures get resolved into local requires
+  scenes = {
+  }
+  for i, scene in ipairs(scenes) do
+    scene:init()
+  end
+  sceneManager:switchScene(nil)
+end
+
+function Game:load(game_updater)
+  -- move to constructor
+  self.game_updater = game_updater
+  local user_input_conf = read_key_file()
+  if user_input_conf then
+    self.input.inputConfigurations = user_input_conf
+  end
+end
+
+
+
+-- Called every few fractions of a second to update the game
+-- dt is the amount of time in seconds that has passed.
+function Game:update(dt)
+  if love.mouse.getX() == self.last_x and love.mouse.getY() == self.last_y then
+    if not self.pointer_hidden then
+      if self.input_delta > consts.MOUSE_POINTER_TIMEOUT then
+        self.pointer_hidden = true
+        love.mouse.setVisible(false)
+      else
+        self.input_delta = self.input_delta + dt
+      end
+    end
+  else
+    self.last_x = love.mouse.getX()
+    self.last_y = love.mouse.getY()
+    self.input_delta = 0.0
+    if self.pointer_hidden then
+      self.pointer_hidden = false
+      love.mouse.setVisible(true)
+    end
+  end
+
+  leftover_time = leftover_time + dt
+  
+  if self.backgroundImage then
+    self.backgroundImage:update(dt)
+  end
+  
+  local newPixelWidth, newPixelHeight = love.graphics.getWidth(), love.graphics.getHeight()
+  if GAME.previousWindowWidth ~= newPixelWidth then
+    GAME:updateCanvasPositionAndScale(newPixelWidth, newPixelHeight)
+    if GAME.match then
+      GAME.needsAssetReload = true
+    else
+      GAME:refreshCanvasAndImagesForNewScale()
+    end
+    GAME.showGameScale = true
+  end
+  
+  local status, err = nil
+  if coroutine.status(self.setup) ~= "dead" then
+    status, err = coroutine.resume(self.setup)
+  elseif sceneManager.active_scene then
+    sceneManager.active_scene:update(dt)
+    -- update transition to use draw priority queue
+    if sceneManager.is_transitioning then
+      sceneManager:transition()
+    end
+    status = true
+  elseif sceneManager.is_transitioning then
+    sceneManager:transition()
+    status = true
+  else
+    status, err = coroutine.resume(mainloop)
+  end
+  if not status then
+    local errorData = self:errorData(err, debug.traceback(mainloop))
+    if GAME_UPDATER_GAME_VERSION then
+      send_error_report(errorData)
+    end
+    error(err .. "\n\n" .. dump(errorData))
+  end
+  if self.server_queue and self.server_queue:size() > 0 then
+    logger.trace("Queue Size: " .. self.server_queue:size() .. " Data:" .. self.server_queue:to_short_string())
+  end
+  this_frame_messages = {}
+
+  update_music()
+end
+
+function Game:draw()
+  -- if not main_font then
+  -- main_font = love.graphics.newFont("Oswald-Light.ttf", 15)
+  -- end
+  -- main_font:setLineHeight(0.66)
+  -- love.graphics.setFont(main_font)
+  if self.foreground_overlay then
+    local scale = consts.CANVAS_WIDTH / math.max(self.foreground_overlay:getWidth(), self.foreground_overlay:getHeight()) -- keep image ratio
+    menu_drawf(self.foreground_overlay, consts.CANVAS_WIDTH / 2, consts.CANVAS_HEIGHT / 2, "center", "center", 0, scale, scale)
+  end
+
+  -- Clear the screen
+  love.graphics.setBlendMode("alpha", "alphamultiply")
+  love.graphics.setCanvas(self.globalCanvas)
+  love.graphics.setBackgroundColor(unpack(global_background_color))
+  love.graphics.clear()
+
+  if STONER_MODE then
+    gprintf("STONER", 1, 1 + (11 * 4))
+  end
+
+  for i = self.gfx_q.first, self.gfx_q.last do
+    self.gfx_q[i][1](unpack(self.gfx_q[i][2]))
+  end
+  self.gfx_q:clear()
+  
+  -- Draw the FPS if enabled
+  if self.config.show_fps then
+    love.graphics.print("FPS: " .. love.timer.getFPS(), 1, 1)
+  end
+  
+  if self.showGameScale or config.debug_mode then
+    local scaleString = "Scale: " .. self.canvasXScale .. " (" .. canvas_width * self.canvasXScale .. " x " .. canvas_height * self.canvasYScale .. ")"
+    local newPixelWidth = love.graphics.getWidth()
+
+    if canvas_width * self.canvasXScale > newPixelWidth then
+      scaleString = scaleString .. " Clipped "
+    end
+    love.graphics.printf(scaleString, get_global_font_with_size(30), 5, 5, 2000, "left")
+  end
+
+  love.graphics.setCanvas() -- render everything thats been added
+  love.graphics.clear(love.graphics.getBackgroundColor()) -- clear in preperation for the next render
+  
+  x, y, w, h = scale_letterbox(love.graphics.getWidth(), love.graphics.getHeight(), 16, 9)
+  love.graphics.setBlendMode("alpha", "premultiplied")
+  love.graphics.draw(self.globalCanvas, self.canvasX, self.canvasY, 0, self.canvasXScale, self.canvasYScale)
+
+  -- draw background and its overlay
+  if self.backgroundImage then
+    self.backgroundImage:draw()
+  end
+  
+  if self.background_overlay then
+    local scale = consts.CANVAS_WIDTH / math.max(self.background_overlay:getWidth(), self.background_overlay:getHeight()) -- keep image ratio
+    menu_drawf(self.background_overlay, consts.CANVAS_WIDTH / 2, consts.CANVAS_HEIGHT / 2, "center", "center", 0, scale, scale)
+  end
+end
+
+function Game:clearMatch()
   self.match = nil
   self.gameIsPaused = false
   self.renderDuringPause = false
@@ -157,6 +384,4 @@ function Game:drawLoadingString(loadingString)
   gprintf(loadingString, x, y, canvas_width, "center", nil, nil, 10)
 end
 
-local game = Game()
-
-return game
+return Game
---
+++ scenes/sceneManager.lua
@@ -0,0 +1,60 @@
+local consts = require("consts")
+local socket = require("socket")
+local transitionUtils = require("scenes.transitionUtils")
+
+--@module scene_manager
+local scene_manager = {
+  active_scene = nil,
+  next_scene = nil,
+  is_transitioning = false
+}
+
+local scenes = {}
+local transition_co = nil
+local transition_type = "fade"
+local transitions = {
+  none = {
+    pre_load_transition = function() end,
+    post_load_transition = function() end
+  },
+  fade = {
+    pre_load_transition = function() transitionUtils.fade(0, 1, .2) end,
+    post_load_transition = function() transitionUtils.fade(1, 0, .2) end
+  }
+}
+
+function scene_manager:switchScene(scene_name, scene_params)
+  transition_co = coroutine.create(function() self:transitionFn(scene_params) end)
+  self.next_scene = scenes[scene_name]
+  self.is_transitioning = true
+end
+
+function scene_manager:transitionFn(scene_params)
+  transitions[transition_type].pre_load_transition()
+  
+  if scene_manager.active_scene then
+    self.active_scene:unload()
+  end
+  
+  if self.next_scene then
+    self.next_scene:load(scene_params)
+    self.active_scene = self.next_scene
+    GAME.rich_presence:setPresence(nil, self.next_scene.name, true)
+  else
+    self.active_scene = nil
+  end
+  
+  transitions[transition_type].post_load_transition()
+  
+  self.is_transitioning = false
+end
+
+function scene_manager:transition()
+  coroutine.resume(transition_co)
+end
+
+function scene_manager:addScene(scene)
+  scenes[scene.name] = scene
+end
+
+return scene_manager
