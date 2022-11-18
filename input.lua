2f8d3438691002d6459b7ef9f4bb72dd4f0a6e01
Shosoul <18148657+Shosoul@users.noreply.github.com>
2022-11-18 11:13:05 -0500

    Allow Client-Side Modification of Win Counts
    See #668

--- input.lua
+++ input.lua
@@ -232,6 +232,7 @@ function love.keypressed(key, scancode, rep)
       love.graphics.captureScreenshot("screenshots/" .. filename)
       return true
     end
+    
   end
 
   local function handleCopy()
@@ -272,11 +273,29 @@ function love.keypressed(key, scancode, rep)
       end
     end
   end
+
+  local function modifyWinCounts()
+    if GAME.battleRoom and (love.keyboard.isDown("lalt") or love.keyboard.isDown("ralt")) then
+      if key == "1" then -- Add to P1's win count
+        GAME.battleRoom.playerWinCounts[1] = math.max(0, GAME.battleRoom.playerWinCounts[1] + 1)
+      end
+      if key == "2" then -- Subtract from P1's win count
+        GAME.battleRoom.playerWinCounts[1] = math.max(0, GAME.battleRoom.playerWinCounts[1] - 1)
+      end
+      if key == "3" then -- Add to P2's win count
+        GAME.battleRoom.playerWinCounts[2] = math.max(0, GAME.battleRoom.playerWinCounts[2] + 1)
+      end
+      if key == "4" then -- Subtract from P2's win count
+        GAME.battleRoom.playerWinCounts[2] = math.max(0, GAME.battleRoom.playerWinCounts[2] - 1)
+      end
+    end
+  end
   
   if handleFullscreenToggle() or
      handleScreenshot() or
      handleCopy() or
-     handleDumpAttackPattern() then
+     handleDumpAttackPattern() or
+     modifyWinCounts() then
     return
   end
 
