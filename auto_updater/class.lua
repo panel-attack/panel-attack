03fc27e18cd2520553a208fc490aee3c19ca0aab
Endaris <Eragon@gmx.net>
2022-11-25 02:40:28 -0600

    draft for list subclass

--- auto_updater/class.lua
+++ auto_updater/class.lua
@@ -10,3 +10,21 @@ function class(init)
   setmetatable(c, mt)
   return c
 end
+
+--@module class
+local class = function(init, parent)
+  local class_tbl = {}
+  local metatable = {
+    __index = parent,
+    __call = function(_, ...)
+      local self = parent and parent(...) or {}
+      setmetatable(self, { __index = class_tbl })
+      init(self, ...)
+      return self
+    end
+  }
+  setmetatable(class_tbl, metatable)
+  return class_tbl
+end
+
+return class
--- table_util.lua
+++ table_util.lua
@@ -22,17 +22,9 @@ end
 function table.filter(tab, filter)
   local filteredTable = {}
 
-  if table.isList(tab) then
-    for i = 1, #tab do
-      if filter(tab[i]) then
-        table.insert(filteredTable, tab[i])
-      end
-    end
-  else
-    for key, value in pairs(tab) do
-      if filter(value) then
-        filteredTable[key] = value
-      end
+  for key, value in pairs(tab) do
+    if filter(value) then
+      filteredTable[key] = value
     end
   end
 
@@ -59,62 +51,26 @@ function table.trueForAll(tab, condition)
   return true
 end
 
--- appends all entries of tab to the end of list
-function table.appendToList(list, tab)
-  assert(table.isList(tab), "insertListAt can only be used with continuously integer indexed tables")
-  for i = 1, #tab do
-    list[#list+1] = tab[i]
-  end
-end
-
--- inserts all entries of tab starting at the specified position of list
-function table.insertListAt(list, position, tab)
-  assert(table.isList(tab), "insertListAt can only be used with continuously integer indexed tables")
-  for i = #tab, 1, -1 do
-    table.insert(list, position, tab[i])
-  end
-end
-
 -- returns true if the table contains the given element, otherwise false
 function table.contains(tab, element)
-  return table.trueForAny(
-    tab,
+  return tab:trueForAny(
     function(tabElement)
       return deep_content_equal(tabElement, element)
     end
   )
 end
 
--- appends an element to a table only if it does not contain the element yet
---
--- use this when you want to pretend that your table is a hashset
-function table.appendIfNotExists(tab, element)
-  if not table.contains(tab, element) then
-    table.insert(tab, #tab + 1, element)
-  end
-end
-
 -- Randomly grabs a value from the table
 function table.getRandomElement(tab)
-  if table.isList(tab) then
-    return tab[math.random(#tab)]
-  else
-    -- pairs already returns in an arbitrary order but I'm not sure if it's truly random
-    local rolledIndex = math.random(table.length(tab))
-    local index = 0
-    for _, value in pairs(tab) do
-      index = index + 1
-      if index == rolledIndex then
-        return value
-      end
+  -- pairs already returns in an arbitrary order but I'm not sure if it's truly random
+  local rolledIndex = math.random(table.length(tab))
+  local index = 0
+  for _, value in pairs(tab) do
+    index = index + 1
+    if index == rolledIndex then
+      return value
     end
   end
-end
-
--- returns true if the table uses sequential integer indices without gaps
-function table.isList(tab)
-  -- the tab[1] check is just to get dictionaries out of the way really quickly
-  return tab[1] ~= nil and #tab == table.length(tab)
 end
 
 -- returns all keys of a table, sorted using the standard comparator to account for sequence based tables
@@ -137,4 +93,113 @@ function table.indexOf(tab, element)
   end
 
   return nil
+end
+
+
+-- a list is formerly defined as a table with monotonously increasing integer based keys starting at 1 so that #tab computes properly
+-- the list may also start at index 0 which has to be indicated upon construction by setting zeroIndexed = true
+List = class(function(self, zeroIndexed)
+  self.isList = true
+  if zeroIndexed == true then
+    self.firstIndex = 0
+  else
+    self.firstIndex = 1
+  end
+end,
+table)
+
+-- returns the index of the first occurence of element in the table
+function List.indexOf(list, element)
+  for i = list.firstIndex, #list do
+    if list[i] == element then
+      return i
+    end if
+  end
+end
+
+-- the length of the list, accounting for the first index potentially being 0 instead
+function List.length(list)
+    return #list + (1 - self.firstIndex)
+end
+
+function List.trueForAny(list, condition)
+  for i = list.firstIndex, #list do
+    if condition(list[i]) then
+      return true
+    end
+  end
+  return false
+end
+
+function List.trueForAll(list, condition)
+  for i = list.firstIndex, #list do
+    if not condition(list[i]) then
+      return false
+    end
+  end
+  return true
+end
+
+function List.append(list, element)
+  list[list:length() + list.firstIndex] = element
+end
+
+-- appends all entries of tab to the end of list
+function List.appendList(list, tab)
+  for i = list.firstIndex, #tab do
+    list:append(element)
+  end
+end
+
+-- appends an element to a table only if it does not contain the element yet
+--
+-- use this when you want to pretend that your table is a hashset
+function List.appendIfNotExists(list, element)
+  if not list:contains(element) then
+    list:append(element)
+  end
+end
+
+-- inserts all entries of the list starting at the specified position of list
+function List.insertAt(list, position, listToInsert)
+  for i = #listToInsert, listToInsert.firstIndex, -1 do
+    table.insert(list, position, listToInsert[i])
+  end
+end
+
+-- returns true if the table contains the given element, otherwise false
+function List.contains(list, element)
+  return list:trueForAny(
+    function(listElement)
+      return deep_content_equal(listElement, element)
+    end
+  )
+end
+
+-- returns all elements in a new list starting at index 1 that fulfill the filter condition
+function List.filter(list, filter)
+  local filteredList = List()
+
+  for i = list.firstIndex, #list do
+    if filter(list[i]) then
+      filteredList[#filteredList + 1] = list[i]
+    end
+  end
+
+  return filteredList
+end
+
+-- returns a new list where the value of each pair is replaced by the return value of running it through the supplied function
+function List.map(list, func)
+  local mappedList = List(list.firstIndex == 0)
+  
+  for i = list.firstIndex, list:length() do
+    mappedList[i] = func(list[i])
+  end
+  
+  return mappedList
+end
+
+function List.getRandomElement(list)
+  return list[math.random(#list)]
 end
