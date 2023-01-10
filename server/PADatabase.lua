32ad0ca80c1550aab98e827bbb555f5c79335749
Shosoul <dirtbikematt12@gmail.com>
2023-01-10 12:26:55 -0500

    Reformat for basic ELO sample implementation

---
+++ server/PADatabase.lua
@@ -0,0 +1,95 @@
+local sqlite3 = require("lsqlite3")
+local db = sqlite3.open("PADatabase.sqlite3")
+
+PADatabase =
+  class(
+  function(self)
+  end
+)
+
+db:exec[[
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS Player(
+  publicPlayerID INTEGER PRIMARY KEY AUTOINCREMENT,
+  privatePlayerID INTEGER NOT NULL UNIQUE,
+  username TEXT NOT NULL,
+  rating REAL NOT NULL DEFAULT 0,
+  placementDone BOOLEAN NOT NULL CHECK (rating IN (0, 1)) DEFAULT 0,
+  placementRating REAL NOT NULL DEFAULT 0,
+  rankedGamesPlayed INTEGER NOT NULL DEFAULT 0,
+  rankedGamesWon INTEGER NOT NULL DEFAULT 0,
+  lastLoginTime TIME TIMESTAMP DEFAULT (strftime('%s', 'now'))
+);
+
+CREATE TABLE IF NOT EXISTS PlayerELOHistory(
+  publicPlayerID INTEGER,
+  rating REAL NOT NULL,
+  updateTime TIME TIMESTAMP DEFAULT (strftime('%s', 'now')),
+  FOREIGN KEY(publicPlayerID) REFERENCES Player(publicPlayerID)
+);
+]]
+--[[
+DROP TABLE IF EXISTS PlayerGameResult;
+DROP TABLE IF EXISTS Game;
+
+CREATE TABLE IF NOT EXISTS Game(
+  gameID INTEGER PRIMARY KEY NOT NULL,
+  ranked BOOLEAN NOT NULL CHECK (ranked IN (0, 1)),
+  timePlayed TIME TIMESTAMP NOT NULL DEFAULT (strftime('%s', 'now'))
+);
+
+CREATE TABLE IF NOT EXISTS PlayerGameResult(
+  playerID INTEGER NOT NULL,
+  gameID INTEGER NOT NULL,
+  level INTEGER NOT NULL,
+  placement INTEGER NOT NULL,
+  FOREIGN KEY(playerID) REFERENCES Player(publicPlayerID),
+  FOREIGN KEY(gameID) REFERENCES Game(gameID)
+);
+]]
+--local selectLeaderboardStatement = assert(db:prepare("SELECT username, rating FROM Player"))
+--local insertGameStatement = assert(db:prepare("INSERT INTO Game(gameID, ranked) VALUES (?, ?)"))
+
+-- TODO: Make work with foreign keys
+--local insertPlayerGameResultStatement = assert(db:prepare("INSERT INTO PlayerGameResult(playerID, gameID, level, placement) VALUES (?, ?, ?, ?)"))
+
+--local selectPlayerGamesStatement = assert(db:prepare("SELECT gameID FROM PlayerGameResult WHERE playerID = ?"))
+
+local insertPlayerStatement = assert(db:prepare("INSERT OR IGNORE INTO Player(privatePlayerID, username) VALUES (?, ?)"))
+function PADatabase.insertNewPlayer(privatePlayerID, username)
+  insertPlayerStatement:bind_values(privatePlayerID, username)
+  insertPlayerStatement:step()
+  if insertPlayerStatement:reset() ~= 0 then
+    print(db:errmsg())
+  end
+end
+
+local updatePlayerRatingStatement = assert(db:prepare("UPDATE Player SET rating = ? WHERE privatePlayerID = ?"))
+function PADatabase.updatePlayerRating(privatePlayerID, newRating)
+  updatePlayerRatingStatement:bind_values(newRating, privatePlayerID)
+  updatePlayerRatingStatement:step()
+  if updatePlayerRatingStatement:reset() ~= 0 then
+    print(db:errmsg())
+  end
+end
+
+local updatePlayerUsernameStatement = assert(db:prepare("UPDATE Player SET username = ? WHERE privatePlayerID = ?"))
+function PADatabase.updatePlayerUsername(privatePlayerID, username)
+  updatePlayerUsernameStatement:bind_values(privatePlayerID, username)
+  updatePlayerUsernameStatement:step()
+  if updatePlayerUsernameStatement:reset() ~= 0 then
+    print(db:errmsg())
+  end
+end
+
+local insertPlayerELOChangeStatement = assert(db:prepare("INSERT INTO PlayerELOHistory(publicPlayerID, rating) VALUES (?, ?)"))
+function PADatabase.insertPlayerELOChange(publicPlayerID, rating)
+  insertPlayerELOChangeStatement:bind_values(publicPlayerID, rating)
+  insertPlayerELOChangeStatement:step()
+  if insertPlayerELOChangeStatement ~= 0 then
+    print(db:errmsg())
+  end
+end
+
+return PADatabase
--- server/createpadatabase.sql
+++ server/createpadatabase.sql
@@ -2,21 +2,18 @@ DROP TABLE IF EXISTS PlayerGameResult;
 DROP TABLE IF EXISTS Game;
 DROP TABLE IF EXISTS Player;
 
-PRAGMA foreign_keys = ON;
+PRAGMA foreign_keys = OFF;
 
 CREATE TABLE IF NOT EXISTS Player(
   privatePlayerID INTEGER PRIMARY KEY NOT NULL,
-  publicPlayerID INTEGER,
+  publicPlayerID INTEGER NOT NULL,
   username TEXT NOT NULL,
   rating REAL NOT NULL DEFAULT 0,
   placementDone BOOLEAN NOT NULL CHECK (rating IN (0, 1)) DEFAULT 0,
   placementRating REAL NOT NULL DEFAULT 0,
   rankedGamesPlayed INTEGER NOT NULL DEFAULT 0,
   rankedGamesWon INTEGER NOT NULL DEFAULT 0,
-  lastLoginTime TIME TIMESTAMP DEFAULT (strftime('%s', 'now')),
-  developer BOOLEAN CHECK (developer IN (0, 1)),
-  youtuber BOOLEAN CHECK (youtuber IN (0, 1)),
-  twitchStreamer BOOLEAN CHECK (twitchStreamer IN (0, 1))
+  lastLoginTime TIME TIMESTAMP DEFAULT (strftime('%s', 'now'))
 );
 
 CREATE TABLE IF NOT EXISTS Game(
@@ -32,3 +29,4 @@ CREATE TABLE IF NOT EXISTS PlayerGameResult(
   placement INTEGER NOT NULL,
   FOREIGN KEY(playerID) REFERENCES Player(publicPlayerID),
   FOREIGN KEY(gameID) REFERENCES Game(gameID)
+);
