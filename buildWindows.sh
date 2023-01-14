e2b5244e6a00934a36dae028905b24fab24ae374
Endaris <Eragon@gmx.net>
2023-01-14 15:58:44 -0600

    make embedded build script assume the existence of a love folder with the required files

--- buildWindows.sh
+++ buildWindows.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script assumes you are running it on a Linux machine 
+# It should be possible to run as well in git bash under windows after adding some of the commands to it
 rm panel*.love panel.zip panel.exe auto_updater/panel*.love
 zip -rq panel-attack.love *.* README THANKS COPYING characters computerPlayers default_data engine panels rich_presence select_screen stages themes
 mv panel-attack.love auto_updater/panel-attack.love
@@ -7,19 +8,11 @@ cd auto_updater
 zip -r ../panel-attack.love *
 cd ..
 mkdir -p love
+# Your panel-attack repository needs to have a directory called love
+# That directory is expected to contain the contents of the unzipped 32-bit windows version of love downloaded from the love2d mainpage
 mv panel-attack.love love/panel-attack.love
 echo "Build windows exe"
 
-# Provide a path to the contents of the unzipped 32-bit windows version of love downloaded from the love2d mainpage
-cp '/home/florian/Apps/love-11.4-win32/SDL2.dll' love
-cp '/home/florian/Apps/love-11.4-win32/OpenAL32.dll' love
-cp '/home/florian/Apps/love-11.4-win32/love.dll' love
-cp '/home/florian/Apps/love-11.4-win32/lua51.dll' love
-cp '/home/florian/Apps/love-11.4-win32/mpg123.dll' love
-cp '/home/florian/Apps/love-11.4-win32/msvcp120.dll' love
-cp '/home/florian/Apps/love-11.4-win32/msvcr120.dll' love
-cp '/home/florian/Apps/love-11.4-win32/license.txt' love
-cp '/home/florian/Apps/love-11.4-win32/love.exe' love
 cd ./love
 cat love.exe panel-attack.love > panel.exe
 cp panel.exe ../panel.love
