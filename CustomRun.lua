10808b7448aa3d6fb321e07a04c283d0e9b9ff41
Endaris <Eragon@gmx.net>
2023-02-25 15:29:03 -0600

    removed memory boundary

--- CustomRun.lua
+++ CustomRun.lua
@@ -35,10 +35,8 @@ function CustomRun.sleep()
 
   local idleTime = targetTime - currentTime
   -- actively collecting garbage is very CPU intensive
-  -- only do it while the game has uncharacteristally high memory (near equivalent with active rollback)
-  if collectgarbage("count") / 1024 > 20 and
-  -- and while a match is on-going
-    (GAME and GAME.match and GAME.focused and not GAME.gameIsPaused) then
+  -- only do it while a match is on-going
+  if (GAME and GAME.match and GAME.focused and not GAME.gameIsPaused) then
     -- Spend as much time as necessary collecting garbage, but at least 0.1ms
     -- manualGc itself has a ceiling at which it will stop
     manualGc(math.max(0.0001, idleTime * 0.99))
