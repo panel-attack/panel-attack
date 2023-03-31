f68cfb79ea9512d1dcabebdda3ad02efd2943924
Endaris <Endaris@users.noreply.github.com>
2023-03-30 21:52:14 -0600

    Fix training mode garbage drops (#888)
    * fix training mode garbage drops
    
    * reflowed shouldDropGarbage in a way that actual n-height combo garbage doesn't get stalled indefinitely outside of training mode
    
    ---------
    
    Co-authored-by: Endaris <Eragon@gmx.net>

--- engine.lua
+++ engine.lua
@@ -1268,14 +1268,17 @@ function Stack.shouldDropGarbage(self)
   -- new garbage can't drop if the stack is full
   -- new garbage always drops one by one
   if not self.panels_in_top_row and not self:has_falling_garbage() then
-    if next_garbage_block_height > 1 then
+    if not self:hasActivePanels() then
+      return true
+    elseif from_chain then
       -- drop chain garbage higher than 1 row immediately
-      return from_chain
-      -- there is a gap here for combo garbage higher than 1 but unless you implement a meme mode,
-      -- that doesn't exist anyway
+      return next_garbage_block_height > 1
     else
-      -- otherwise garbage should only be dropped if there are no active panels
-      return not self:hasActivePanels()
+      -- attackengine garbage higher than 1 (aka chain garbage) is treated as combo garbage
+      -- that is to circumvent the garbage queue not allowing to send multiple chains simultaneously
+      -- and because of that hack, we need to do another hack here and allow n-height combo garbage
+      -- but only if trainingmodesettings have been set on the GAME global
+      return next_garbage_block_height > 1 and GAME.battleRoom.trainingModeSettings ~= nil
     end
   end
 end
