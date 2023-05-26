9945e61337b538761ac8b47817ba57f96bd759d2
Sam Ekong <samekong@gmail.com>
2023-05-25 18:22:46 -0700

    Copied existing rewind mechanism (seems broken)

--- scenes/replayGame.lua
+++ scenes/replayGame.lua
@@ -34,20 +34,31 @@ function replayGame:customRun()
       GAME.match.P2.max_runs_per_frame = 1
     end
   elseif input:isPressedWithRepeat("Right", consts.KEY_DELAY, consts.KEY_REPEAT_PERIOD) then
-    self.playbackSpeed = util.bound(1, self.playbackSpeed + 1, self.maximumSpeed)
+    self.playbackSpeed = util.bound(-1, self.playbackSpeed + 1, self.maximumSpeed)
     if GAME.match.P1 then
-      GAME.match.P1.max_runs_per_frame = self.playbackSpeed
+      GAME.match.P1.max_runs_per_frame = math.max(self.playbackSpeed, 0)
     end
     if GAME.match.P2 then
-      GAME.match.P2.max_runs_per_frame = self.playbackSpeed
+      GAME.match.P2.max_runs_per_frame = math.max(self.playbackSpeed, 0)
     end
   elseif input:isPressedWithRepeat("Left", consts.KEY_DELAY, consts.KEY_REPEAT_PERIOD) then
-    self.playbackSpeed = util.bound(1, self.playbackSpeed - 1, self.maximumSpeed)
+    self.playbackSpeed = util.bound(-1, self.playbackSpeed - 1, self.maximumSpeed)
     if GAME.match.P1 then
-      GAME.match.P1.max_runs_per_frame = self.playbackSpeed
+      GAME.match.P1.max_runs_per_frame = math.max(self.playbackSpeed, 0)
     end
     if GAME.match.P2 then
-      GAME.match.P2.max_runs_per_frame = self.playbackSpeed
+      GAME.match.P2.max_runs_per_frame = math.max(self.playbackSpeed, 0)
+    end
+  end
+  
+  if self.playbackSpeed == -1 then
+    if GAME.match.P1 and GAME.match.P1.CLOCK > 0 and GAME.match.P1.prev_states[GAME.match.P1.CLOCK-1] then
+      GAME.match.P1:rollbackToFrame(GAME.match.P1.CLOCK-1)
+      GAME.match.P1.lastRollbackFrame = -1 -- We don't want to count this as a "rollback" because we don't want to catchup
+    end
+    if GAME.match.P2 and GAME.match.P2.CLOCK > 0 and GAME.match.P2.prev_states[P2.CLOCK-1] then
+      GAME.match.P2:rollbackToFrame(GAME.match.P2.CLOCK-1)
+      GAME.match.P2.lastRollbackFrame = -1 -- We don't want to count this as a "rollback" because we don't want to catchup
     end
   end
 end
