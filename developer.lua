a485f15dc0d67ba009017a7633b055aabd9123de
JamBox <8935453+JamesVanBoxtel@users.noreply.github.com>
2023-07-08 16:19:02 -0700

    More specific command line arg

--- developer.lua
+++ developer.lua
@@ -11,7 +11,9 @@ for _, value in pairs(arg) do
     PROFILING_ENABLED = 1
   elseif value == "performanceTests" then
     PERFORMANCE_TESTS_ENABLED = 1
-  elseif value ~= "love" and value ~= "embedded boot.lua" and value ~= "." then
-    CUSTOM_USER_ID = value
+  else
+    for match in string.gmatch(value, "user%-id=(.*)") do
+      CUSTOM_USER_ID = match
+    end
   end
 end
