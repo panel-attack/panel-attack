1107adb50646486c8d6a3f9ab2e88d19396fa593
Endaris <Endaris@users.noreply.github.com>
2023-10-10 02:24:22 +0200

    almost figured it out I think

--- graphics.lua
+++ graphics.lua
@@ -131,7 +131,7 @@ function Stack:drawLabel(drawable, themePositionOffset, scale, cameFromLegacySco
   menu_drawf(drawable, x, y, "left", "left", 0, scale, scale)
 end
 
-function Stack:drawBar(image, quad, x, y, height, rotate, scale)
+function Stack:drawBar(image, quad, themePositionOffset, height, yOffset, rotate, scale)
   local imageWidth = image:getWidth()
   local imageHeight = image:getHeight()
   local barYScale = height / imageHeight
@@ -140,8 +140,10 @@ function Stack:drawBar(image, quad, x, y, height, rotate, scale)
     barYScale = 1
     quadY = imageHeight - height
   end
+  local x = self:elementOriginXWithOffset(themePositionOffset, false)
+  local y = self:elementOriginYWithOffset(themePositionOffset, false)
   quad:setViewport(0, quadY, imageWidth, imageHeight - quadY)
-  qdraw(image, quad, x / GFX_SCALE, (y - height) / GFX_SCALE, rotate, scale, scale * barYScale)
+  qdraw(image, quad, x / GFX_SCALE, (y - height) / GFX_SCALE - yOffset, rotate, scale / GFX_SCALE, scale * barYScale / GFX_SCALE, 0, 0, self.mirror_x)
 end
 
 function Stack:drawNumber(number, quads, themePositionOffset, scale, cameFromLegacyScoreOffset)
@@ -764,18 +766,15 @@ function Stack.render(self)
       shake_time = self.puzzle.shake_time
     end
 
-    -- If we have a healthbar frame draw it.
-    if self.theme.images["IMG_healthbar_frame" .. self.id] then
-      self:drawLabel(self.theme.images["IMG_healthbar_frame" .. self.id], self.theme.healthbar_frame_Pos, self.theme.healthbar_frame_Scale)
-    end
+    
+    self:drawLabel(self.theme.images["IMG_healthbar_frame" .. self.id], self.theme.healthbar_frame_Pos, self.theme.healthbar_frame_Scale)
 
     local multiBarFrameCount = self.multiBarFrameCount
     local multiBarMaxHeight = 590 * self.theme.multibar_Scale
-    local multiBarX = self:elementOriginXWithOffset(self.theme.multibar_Pos, false)
     local multiBarBottomY = self:elementOriginYWithOffset(self.theme.multibar_Pos, false) + multiBarMaxHeight
 
     local healthHeight = (self.health / multiBarFrameCount) * multiBarMaxHeight
-    self:drawBar(self.theme.images.IMG_healthbar, self.healthQuad, multiBarX, multiBarBottomY - healthHeight, healthHeight, 0, self.theme.multibar_Scale)
+    self:drawBar(self.theme.images.IMG_healthbar, self.healthQuad, self.theme.multibar_Pos, healthHeight, 0, 0, self.theme.multibar_Scale)
 
     multiBarBottomY = multiBarBottomY - healthHeight
 
@@ -786,12 +785,14 @@ function Stack.render(self)
       -- shake is only drawn if it is greater than prestop + stop
       -- shake is always guaranteed to fit
       local shakeHeight = (shake_time / multiBarFrameCount) * multiBarMaxHeight
-      self:drawBar(self.theme.images.IMG_multibar_shake_bar, self.multi_shakeQuad, multiBarX, multiBarBottomY - shakeHeight, shakeHeight, 0, self.theme.multibar_Scale)
+      self:drawBar(self.theme.images.IMG_multibar_shake_bar, self.multi_shakeQuad, self.theme.multibar_Pos, shakeHeight, 0, 0, self.theme.multibar_Scale)
     else
       -- stop/prestop are only drawn if greater than shake
       if stop_time > 0 then
         stopHeight = math.min(stop_time, multiBarFrameCount - self.health) / multiBarFrameCount * multiBarMaxHeight
-        self:drawBar(self.theme.images.IMG_multibar_stop_bar, self.multi_stopQuad, multiBarX, multiBarBottomY - stopHeight, stopHeight, 0, self.theme.multibar_Scale)
+        self:drawBar(self.theme.images.IMG_multibar_stop_bar, self.multi_stopQuad, self.theme.multibar_Pos, stopHeight, 0, 0, self.theme.multibar_Scale)
+
+        multiBarBottomY = multiBarBottomY - stopHeight
       end
       if self.pre_stop_time > 0 then
         local totalInvincibility = self.health + self.stop_time + self.pre_stop_time
@@ -804,7 +805,7 @@ function Stack.render(self)
           preStopHeight = self.pre_stop_time / multiBarFrameCount * multiBarMaxHeight
         end
 
-        self:drawBar(self.theme.images.IMG_multibar_prestop_bar, self.multi_prestopQuad, multiBarX, multiBarBottomY - preStopHeight, preStopHeight, 0, self.theme.multibar_Scale)
+        self:drawBar(self.theme.images.IMG_multibar_prestop_bar, self.multi_prestopQuad, self.theme.multibar_Pos, preStopHeight, 0, 0, self.theme.multibar_Scale)
 
         if remainingSeconds > 0 then
           self:drawString(tostring(math.floor(remainingSeconds)), {-19, 5}, false, 20)
