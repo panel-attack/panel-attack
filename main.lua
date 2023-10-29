708d689048ba875dc9738eb72d2212854eb4adb9
Endaris <Endaris@users.noreply.github.com>
2023-10-29 12:13:11 +0100

    move remaining code outside of mainloop.lua

--- main.lua
+++ main.lua
@@ -273,6 +273,29 @@ end
 -- click_or_tap(_x, _y, {id = id, x = _x, y = _y, dx = dx, dy = dy, pressure = pressure})
 -- end
 
+-- quit handling
+function love.quit()
+  if PROFILING_ENABLED then
+    GAME.profiler.report("profiler.log")
+  end
+  if network_connected() then
+    json_send({logout = true})
+  end
+  love.audio.stop()
+  if love.window.getFullscreen() then
+    _, _, config.display = love.window.getPosition()
+  else
+    config.windowX, config.windowY, config.display = love.window.getPosition()
+    config.windowX = math.max(config.windowX, 0)
+    config.windowY = math.max(config.windowY, 30) --don't let 'y' be zero, or the title bar will not be visible on next launch.
+  end
+
+  config.windowWidth, config.windowHeight, _ = love.window.getMode( )
+  config.maximizeOnStartup = love.window.isMaximized()
+  config.fullscreen = love.window.getFullscreen()
+  write_conf_file()
+end
+
 function love.errorhandler(msg)
 
   if not love.window or not love.graphics or not love.event then
