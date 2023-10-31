d55a8ac9866261c8c6f0c4b3aec60c33f9bdf90f
Endaris <Endaris@users.noreply.github.com>
2023-10-31 13:19:39 +0100

    remove sleep time check for windows because it's probably not significant after all

--- CustomRun.lua
+++ CustomRun.lua
@@ -55,8 +55,7 @@ function CustomRun.sleep()
   -- Sleep any remaining amount of time to fill up the frametime to 1/60 of a second
   -- On most machines GC will have reduced the remaining idle time to near nothing
   -- But strong machines may exit garbage collection early and need to sleep the remaining time
-  -- On modern windows OS, default sleep is minimum 2ms, so only sleep if there's enough time left
-  if (love.system.getOS() ~= "Windows" and idleTime > 0) or idleTime > 0.002 then
+  if idleTime > 0 then
     love.timer.sleep(idleTime * 0.99)
   end
   currentTime = love.timer.getTime()
