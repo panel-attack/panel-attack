79762c61d4bd3d446c7f3dfa3809663c415f54cc
Endaris <Endaris@users.noreply.github.com>
2023-11-14 20:43:10 +0100

    polishing

--- ModImport.lua
+++ ModImport.lua
@@ -16,19 +16,21 @@ function ModImport.importCharacter(path)
     if not configData then
       error("Error trying to import character " .. path .. "\nCouldn't read config.json\n" .. err)
     else
-      local config = json.decode(configData)
-      if table.contains(characters_ids, config["id"]) then
-        local existingPath = characters[config["id"]].path
+      local modConfig = json.decode(configData)
+      if table.contains(characters_ids, modConfig["id"]) then
+        local existingPath = characters[modConfig["id"]].path
         local backUpPath = ModImport.createBackupDirectory(existingPath)
-        local importFiles = ModImport.recursiveRead(path)
-        local currentFiles = ModImport.recursiveRead(existingPath)
-        ModImport.recursiveCompareBackupAndCopy(importFiles, backUpPath, currentFiles)
+        -- next is just a slightly scuffed way to access the only top level element in the table
+        -- we need to pass without the head as otherwise different folder names can screw the import up
+        local _, importFiles = next(ModImport.recursiveRead(path))
+        local _, currentFiles = next(ModImport.recursiveRead(existingPath))
+        ModImport.recursiveCompareBackupAndCopy(importFiles.files, backUpPath, currentFiles.files)
       else
-        if not lfs.getInfo("characters/" .. config["name"]) then
-					recursive_copy(path, "characters/" .. config["name"])
-				else
-					recursive_copy(path, "characters/" .. config["id"])
-				end
+        if not lfs.getInfo("characters/" .. modConfig["name"]) then
+          recursive_copy(path, "characters/" .. modConfig["name"])
+        else
+          recursive_copy(path, "characters/" .. modConfig["id"])
+        end
       end
 
       return true
@@ -37,7 +39,7 @@ function ModImport.importCharacter(path)
 end
 
 function ModImport.importStage(path)
-	local configPath = path .. "/config.json"
+  local configPath = path .. "/config.json"
   if not lfs.getInfo(configPath, "file") then
     return false
   else
@@ -45,19 +47,21 @@ function ModImport.importStage(path)
     if not configData then
       error("Error trying to import stage " .. path .. "\nCouldn't read config.json\n" .. err)
     else
-      local config = json.decode(configData)
-      if table.contains(stages_ids, config["id"]) then
-        local existingPath = stages[config["id"]].path
+      local modConfig = json.decode(configData)
+      if table.contains(stages_ids, modConfig["id"]) then
+        local existingPath = stages[modConfig["id"]].path
         local backUpPath = ModImport.createBackupDirectory(existingPath)
-        local importFiles = ModImport.recursiveRead(path)
-        local currentFiles = ModImport.recursiveRead(existingPath)
-        ModImport.recursiveCompareBackupAndCopy(importFiles, backUpPath, currentFiles)
+        -- next is just a slightly scuffed way to access the only top level element in the table
+        -- we need to pass without the head as otherwise different folder names can screw the import up
+        local _, importFiles = next(ModImport.recursiveRead(path))
+        local _, currentFiles = next(ModImport.recursiveRead(existingPath))
+        ModImport.recursiveCompareBackupAndCopy(importFiles.files, backUpPath, currentFiles.files)
       else
-				if not lfs.getInfo("stages/" .. config["name"]) then
-					recursive_copy(path, "stages/" .. config["name"])
-				else
-					recursive_copy(path, "stages/" .. config["id"])
-				end
+        if not lfs.getInfo("stages/" .. modConfig["name"]) then
+          recursive_copy(path, "stages/" .. modConfig["name"])
+        else
+          recursive_copy(path, "stages/" .. modConfig["id"])
+        end
       end
 
       return true
@@ -66,7 +70,7 @@ function ModImport.importStage(path)
 end
 
 function ModImport.importPanelSet(path)
-	local configPath = path .. "/config.json"
+  local configPath = path .. "/config.json"
   if not lfs.getInfo(configPath, "file") then
     return false
   else
@@ -74,19 +78,21 @@ function ModImport.importPanelSet(path)
     if not configData then
       error("Error trying to import panels " .. path .. "\nCouldn't read config.json\n" .. err)
     else
-      local config = json.decode(configData)
-      if table.contains(panels_ids, config["id"]) then
-        local existingPath = panels[config["id"]].path
+      local modConfig = json.decode(configData)
+      if table.contains(panels_ids, modConfig["id"]) then
+        local existingPath = panels[modConfig["id"]].path
         local backUpPath = ModImport.createBackupDirectory(existingPath)
-        local importFiles = ModImport.recursiveRead(path)
-        local currentFiles = ModImport.recursiveRead(existingPath)
-        ModImport.recursiveCompareBackupAndCopy(importFiles, backUpPath, currentFiles)
+        -- next is just a slightly scuffed way to access the only top level element in the table
+        -- we need to pass without the head as otherwise different folder names can screw the import up
+        local _, importFiles = next(ModImport.recursiveRead(path))
+        local _, currentFiles = next(ModImport.recursiveRead(existingPath))
+        ModImport.recursiveCompareBackupAndCopy(importFiles.files, backUpPath, currentFiles.files)
       else
-				if not lfs.getInfo("panels/" .. FileUtil.getDirectoryName(path)) then
-					recursive_copy(path, "panels/" .. FileUtil.getDirectoryName(path))
-				else
-					recursive_copy(path, "panels/" .. config["id"])
-				end
+        if not lfs.getInfo("panels/" .. FileUtil.getDirectoryName(path)) then
+          recursive_copy(path, "panels/" .. FileUtil.getDirectoryName(path))
+        else
+          recursive_copy(path, "panels/" .. modConfig["id"])
+        end
       end
 
       return true
@@ -95,7 +101,7 @@ function ModImport.importPanelSet(path)
 end
 
 function ModImport.importTheme(path)
-	local configPath = path .. "/config.json"
+  local configPath = path .. "/config.json"
   if not lfs.getInfo(configPath, "file") then
     return false
   else
@@ -103,15 +109,17 @@ function ModImport.importTheme(path)
     if not configData then
       error("Error trying to import theme " .. path .. "\nCouldn't read config.json\n" .. err)
     else
-			local themeName = FileUtil.getDirectoryName(path)
+      local themeName = FileUtil.getDirectoryName(path)
       if lfs.getInfo("themes/" .. themeName, "directory") then
         local existingPath = "themes/" .. themeName
         local backUpPath = ModImport.createBackupDirectory(existingPath)
         local importFiles = ModImport.recursiveRead(path)
         local currentFiles = ModImport.recursiveRead(existingPath)
+        -- unlike the other mod types, themes are (unfortunately) still identified by foldername
+        -- so we can keep the top level element in (if it wasn't the same, we'd have landed in the else branch)
         ModImport.recursiveCompareBackupAndCopy(importFiles, backUpPath, currentFiles)
       else
-				recursive_copy(path, "themes/" .. themeName)
+        recursive_copy(path, "themes/" .. themeName)
       end
 
       return true
@@ -120,42 +128,38 @@ function ModImport.importTheme(path)
 end
 
 function ModImport.importPuzzleFile(path)
-	-- we really need a proper puzzle format that guarantees identification to some degree
-	-- way too easy to overwrite otherwise compared to themes
+  -- we really need a proper puzzle format that guarantees identification to some degree
+  -- way too easy to overwrite otherwise compared to themes
 end
 
 function ModImport.createBackupDirectory(path)
-	local now = os.date("*t", to_UTC(os.time()))
-	local backUpPath = path .. "/__backup_" ..
-	string.format("%04d-%02d-%02d-%02d-%02d-%02d", now.year, now.month, now.day, now.hour, now.min, now.sec)
-	lfs.createDirectory(backUpPath)
-	return backUpPath
+  local now = os.date("*t", to_UTC(os.time()))
+  local backUpPath = path .. "/__backup_" ..
+                         string.format("%04d-%02d-%02d-%02d-%02d-%02d", now.year, now.month, now.day, now.hour, now.min, now.sec)
+  lfs.createDirectory(backUpPath)
+  return backUpPath
 end
 
 -- This function will recursively populate the passed in empty table fileTree with the directory and fileData
 function ModImport.recursiveRead(folder, fileTree)
-  local function getFileContent(filePath)
-    local fileContent, size = lfs.read(filePath)
-    return {size = size, content = fileContent}
+  if not fileTree then
+    fileTree = {}
   end
-
-	if not fileTree then
-		fileTree = {}
-	end
 
   local filesTable = lfs.getDirectoryItems(folder)
   local folderName = FileUtil.getDirectoryName(folder)
   fileTree[folderName] = {type = "directory", files = {}, path = folder}
   logger.debug("Reading folder " .. folder .. " into memory")
   for _, v in ipairs(filesTable) do
-    local file = folder .. "/" .. v
-    local info = lfs.getInfo(file)
+    local filePath = folder .. "/" .. v
+    local info = lfs.getInfo(filePath)
     if info then
       if info.type == "file" then
-        logger.debug("Reading file " .. file .. " into memory")
-        fileTree[folderName].files[v] = {type = "file", content = getFileContent(file), path = file}
+        logger.debug("Reading file " .. filePath .. " into memory")
+        local fileContent, size = lfs.read(filePath)
+        fileTree[folderName].files[v] = {type = "file", content = {size = size, content = fileContent}, path = filePath}
       elseif info.type == "directory" then
-        ModImport.recursiveRead(file, fileTree[folderName].files)
+        ModImport.recursiveRead(filePath, fileTree[folderName].files)
       end
     end
   end
@@ -167,28 +171,31 @@ function ModImport.recursiveCompareBackupAndCopy(importFiles, backUpPath, curren
   -- assert(droppedFiles.type == "directory")
 
   for key, value in pairs(importFiles) do
-		if value.type == "file" then
-			if not currentFiles[key] then
-				-- the file doesn't exist, we can just copy over
-				copy_file(importFiles[key].path, currentFiles.path .. "/" .. key)
-			elseif value.content.size == currentFiles[key].content.size and value.content.content == currentFiles[key].content.content then
-				-- files are identical, no need to do anything
-			else
-				-- files are not identical, copy the old one to backup before copying the new one over
-				copy_file(currentFiles[key].path, backUpPath .. "/" .. key)
-				copy_file(importFiles[key].path, currentFiles[key].path)
-			end
-		else
-			if currentFiles[key] then
-				local nextBackUpPath = backUpPath .. "/" .. key
-				-- create the path in the backup folder so writes to backup don't fail in the recursive call
-				lfs.createDirectory(nextBackUpPath)
-				return ModImport.recursiveCompareBackupAndCopy(value.files, nextBackUpPath, currentFiles[key].files)
-			else
-				-- the subfolder doesn't exist, we can just copy over
-				recursive_copy(importFiles[key].path, currentFiles.path .. "/" .. key)
-			end
-		end
+    if value.type == "file" then
+      if not currentFiles[key] then
+        -- the file doesn't exist, we can just copy over
+        copy_file(importFiles[key].path, currentFiles.path .. "/" .. key)
+      elseif value.content.size == currentFiles[key].content.size and value.content.content == currentFiles[key].content.content then
+        -- files are identical, no need to do anything
+      else
+        -- files are not identical, copy the old one to backup before copying the new one over
+        copy_file(currentFiles[key].path, backUpPath .. "/" .. key)
+        copy_file(importFiles[key].path, currentFiles[key].path)
+      end
+    else
+      if currentFiles[key] then
+        local nextBackUpPath = backUpPath .. "/" .. key
+        -- create the path in the backup folder so writes to backup don't fail in the recursive call
+        lfs.createDirectory(nextBackUpPath)
+        return ModImport.recursiveCompareBackupAndCopy(value.files, nextBackUpPath, currentFiles[key].files)
+      else
+        -- if currentFiles.path == nil then
+        --   local phi = 5
+        -- end
+        -- the subfolder doesn't exist, we can just copy over
+        recursive_copy(importFiles[key].path, currentFiles.path .. "/" .. key)
+      end
+    end
   end
 end
 
--- main.lua
+++ main.lua
@@ -58,7 +58,6 @@ local logger = require("logger")
 GAME.scores = require("scores")
 GAME.rich_presence = RichPresence()
 
-
 local last_x = 0
 local last_y = 0
 local input_delta = 0.0
@@ -71,7 +70,7 @@ function love.load()
   if PROFILING_ENABLED then
     GAME.profiler:start()
   end
-  
+
   love.graphics.setDefaultFilter("linear", "linear")
   if config.maximizeOnStartup and not love.window.isMaximized() then
     love.window.maximize()
@@ -86,7 +85,7 @@ function love.load()
   GAME.rich_presence:initialize("902897593049301004")
   mainloop = coroutine.create(fmainloop)
 
-  GAME.globalCanvas = love.graphics.newCanvas(canvas_width, canvas_height, {dpiscale=GAME:newCanvasSnappedScale()})
+  GAME.globalCanvas = love.graphics.newCanvas(canvas_width, canvas_height, {dpiscale = GAME:newCanvasSnappedScale()})
 end
 
 function love.focus(f)
@@ -127,7 +126,7 @@ function love.update(dt)
   leftover_time = leftover_time + dt
 
   GAME:update(dt)
-  
+
   if GAME.backgroundImage then
     GAME.backgroundImage:update(dt)
   end
@@ -186,7 +185,7 @@ function love.draw()
     local scale = canvas_width / math.max(GAME.foreground_overlay:getWidth(), GAME.foreground_overlay:getHeight()) -- keep image ratio
     menu_drawf(GAME.foreground_overlay, canvas_width / 2, canvas_height / 2, "center", "center", 0, scale, scale)
   end
-  
+
   -- Draw the FPS if enabled
   if config ~= nil and config.show_fps then
     if not CustomRun.runTimeGraph then
@@ -194,19 +193,20 @@ function love.draw()
     end
   end
 
-  if STONER_MODE then 
+  if STONER_MODE then
     gprintf("Lag Mode On, S:" .. GAME.sendNetworkQueue:length() .. " R:" .. GAME.receiveNetworkQueue:length(), 1, 1 + (11 * 4))
   end
 
   love.graphics.setCanvas() -- render everything thats been added
   love.graphics.clear(love.graphics.getBackgroundColor()) -- clear in preperation for the next render
-    
+
   love.graphics.setBlendMode("alpha", "premultiplied")
   love.graphics.draw(GAME.globalCanvas, GAME.canvasX, GAME.canvasY, 0, GAME.canvasXScale, GAME.canvasYScale)
   love.graphics.setBlendMode("alpha", "alphamultiply")
 
   if GAME.showGameScale or config.debug_mode then
-    local scaleString = "Scale: " .. GAME.canvasXScale .. " (" .. canvas_width * GAME.canvasXScale .. " x " .. canvas_height * GAME.canvasYScale .. ")"
+    local scaleString = "Scale: " .. GAME.canvasXScale .. " (" .. canvas_width * GAME.canvasXScale .. " x " .. canvas_height *
+                            GAME.canvasYScale .. ")"
     local newPixelWidth = love.graphics.getWidth()
 
     if canvas_width * GAME.canvasXScale > newPixelWidth then
@@ -267,7 +267,7 @@ function love.errorhandler(msg)
     end
   end
   local sanitizedTrace = table.concat(traceLines, "\n")
-  
+
   local errorData = Game.errorData(sanitizedMessage, sanitizedTrace)
   local detailedErrorLogString = Game.detailedErrorLogString(errorData)
   errorData.detailedErrorLogString = detailedErrorLogString
@@ -384,14 +384,25 @@ end
 
 function love.filedropped(file)
   if GAME.match == nil then
-    love.filesystem.mount(file:getFilename(), "dropped")
+    local mountPath = "dropped"
+    love.filesystem.mount(file:getFilename(), mountPath)
     -- if a file is a directory, that means it's a zip archive
-    if love.filesystem.getInfo("dropped", "directory") then
-      local subDirectories = FileUtil.getSubDirectories("dropped")
+    if love.filesystem.getInfo(mountPath, "directory") then
+      local path = mountPath
+      local subDirectories = FileUtil.getSubDirectories(path)
+      -- the mod folders need to be either directly at the top level or one below
+      if #subDirectories == 1 then
+        -- verify it's not a single asset type drop
+        if subDirectories[1] ~= "characters" and subDirectories[1] ~= "panels" and subDirectories[1] ~= "stages"
+            and subDirectories[1] ~= "themes" then
+          path = "dropped/" .. subDirectories[1]
+          subDirectories = FileUtil.getSubDirectories(path)
+        end
+      end
       if table.contains(subDirectories, "characters") then
-        local characterDirs = FileUtil.getSubDirectories("dropped/characters")
+        local characterDirs = FileUtil.getSubDirectories(path .. "/characters")
         for i = 1, #characterDirs do
-          if ModImport.importCharacter("dropped/characters/" .. characterDirs[i]) then
+          if ModImport.importCharacter(path .. "/characters/" .. characterDirs[i]) then
             logger.warn("imported character " .. characterDirs[i])
           else
             logger.warn("failed to import character " .. characterDirs[i])
@@ -402,9 +413,9 @@ function love.filedropped(file)
       end
 
       if table.contains(subDirectories, "stages") then
-        local stageDirs = FileUtil.getSubDirectories("dropped/stages")
+        local stageDirs = FileUtil.getSubDirectories(path .."/stages")
         for i = 1, #stageDirs do
-          if ModImport.importStage("dropped/stages/" .. stageDirs[i]) then
+          if ModImport.importStage(path .. "/stages/" .. stageDirs[i]) then
             logger.warn("imported stage " .. stageDirs[i])
           else
             logger.warn("failed to import stage " .. stageDirs[i])
@@ -415,9 +426,9 @@ function love.filedropped(file)
       end
 
       if table.contains(subDirectories, "panels") then
-        local panelDirs = FileUtil.getSubDirectories("dropped/panels")
+        local panelDirs = FileUtil.getSubDirectories(path .. "/panels")
         for i = 1, #panelDirs do
-          if ModImport.importPanelSet("dropped/panels/" .. panelDirs[i]) then
+          if ModImport.importPanelSet(path .. "/panels/" .. panelDirs[i]) then
             logger.warn("imported panels " .. panelDirs[i])
           else
             logger.warn("failed to import panels " .. panelDirs[i])
@@ -428,9 +439,9 @@ function love.filedropped(file)
       end
 
       if table.contains(subDirectories, "themes") then
-        local themeDirs = FileUtil.getSubDirectories("dropped/themes")
+        local themeDirs = FileUtil.getSubDirectories(path .. "/themes")
         for i = 1, #themeDirs do
-          if ModImport.importTheme("dropped/themes/" .. themeDirs[i]) then
+          if ModImport.importTheme(path .. "/themes/" .. themeDirs[i]) then
             logger.warn("imported themes " .. themeDirs[i])
           else
             logger.warn("failed to import themes " .. themeDirs[i])
