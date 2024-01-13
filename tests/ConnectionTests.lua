cc342e59b3cbd617252d351cd0832196e568d082
JamBox <8935453+JamesVanBoxtel@users.noreply.github.com>
2024-01-12 20:00:53 -0800

    Add a network protocol version

--- tests/ConnectionTests.lua
+++ tests/ConnectionTests.lua
@@ -19,27 +19,27 @@ function testLoginInvalidName()
   local connection = Connection(nil, 1, testServer)
   
   -- blank name
-  local denyReason, _ = connection:canLogin(2, nil, "1.1.1.1")
+  local denyReason, _ = connection:canLogin(2, nil, "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 
   -- anonymous
-  denyReason, _ = connection:canLogin(2, "anonymous", "1.1.1.1")
+  denyReason, _ = connection:canLogin(2, "anonymous", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 
   -- Anonymous
-  denyReason, _ = connection:canLogin(2, "Anonymous", "1.1.1.1")
+  denyReason, _ = connection:canLogin(2, "Anonymous", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
     
   -- defaultnam
-  denyReason, _ = connection:canLogin(2, "defaultnam", "1.1.1.1")
+  denyReason, _ = connection:canLogin(2, "defaultnam", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 
   -- only alpha numeric and underscores
-  denyReason, _ = connection:canLogin(2, "L$3t", "1.1.1.1")
+  denyReason, _ = connection:canLogin(2, "L$3t", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 
   -- NAME_LENGTH_LIMIT
-  denyReason, _ = connection:canLogin(2, "testtesttesttesttesttest", "1.1.1.1")
+  denyReason, _ = connection:canLogin(2, "testtesttesttesttesttest", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 end
 
@@ -49,41 +49,50 @@ function testLoginInvalidUserID()
   local connection = Connection(nil, 1, testServer)
   
   -- no user ID
-  local denyReason, _ = connection:canLogin(nil, "Bob", "1.1.1.1")
+  local denyReason, _ = connection:canLogin(nil, "Bob", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 
   -- asking for new user, but name alread taken
-  denyReason, _ = connection:canLogin("need a new user id", "Jerry", "1.1.1.1")
+  denyReason, _ = connection:canLogin("need a new user id", "Jerry", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 
   -- fake ID
-  local _, playerBan = connection:canLogin(42, "Bob", "1.1.1.1")
+  local _, playerBan = connection:canLogin(42, "Bob", "1.1.1.1", ENGINE_VERSION)
   assert(playerBan ~= nil)
 
   -- have account, name taken and doesn't match ID
-  denyReason, _ = connection:canLogin(2, "Jerry", "1.1.1.1")
+  denyReason, _ = connection:canLogin(2, "Jerry", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason ~= nil)
 end
 
 testLoginInvalidUserID()
 
+function testLoginDeniedForInvalidVersion()  
+  local connection = Connection(nil, 1, testServer)
+  
+  local denyReason, playerBan = connection:canLogin(2, "BEN", "1.1.1.1", "XXX")
+  assert(denyReason ~= nil)
+end
+
+testLoginDeniedForInvalidVersion()
+
 function testLoginAllowed()  
   local connection = Connection(nil, 1, testServer)
   
   -- can login if have account and username case changed
-  local denyReason, playerBan = connection:canLogin(2, "BEN", "1.1.1.1")
+  local denyReason, playerBan = connection:canLogin(2, "BEN", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason == nil and playerBan == nil)
 
   -- can login if have account and changes name
-  denyReason, playerBan = connection:canLogin(2, "Jeremy", "1.1.1.1")
+  denyReason, playerBan = connection:canLogin(2, "Jeremy", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason == nil and playerBan == nil)
 
   -- can login if have account and name isn't changed
-  denyReason, playerBan = connection:canLogin(2, "Ben", "1.1.1.1")
+  denyReason, playerBan = connection:canLogin(2, "Ben", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason == nil and playerBan == nil)
 
   -- can login with new account if name not taken
-  denyReason, playerBan = connection:canLogin("need a new user id", "Joseph", "1.1.1.1")
+  denyReason, playerBan = connection:canLogin("need a new user id", "Joseph", "1.1.1.1", ENGINE_VERSION)
   assert(denyReason == nil and playerBan == nil)
   
 end
