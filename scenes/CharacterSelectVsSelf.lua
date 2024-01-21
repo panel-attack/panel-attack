3624690560000b65f000c723fa7bbec12a0f537b
Endaris <Endaris@users.noreply.github.com>
2024-01-21 23:28:55 +0100

    add a PixelFontLabel class to draw pixel font labels and use them for the records display in 1p vs self

--- scenes/CharacterSelectVsSelf.lua
+++ scenes/CharacterSelectVsSelf.lua
@@ -4,6 +4,9 @@ local class = require("class")
 local GameModes = require("GameModes")
 local Grid = require("ui.Grid")
 local MultiPlayerSelectionWrapper = require("ui.MultiPlayerSelectionWrapper")
+local UiElement = require("ui.UIElement")
+local PixelFontLabel = require("ui.PixelFontLabel")
+local StackPanel = require("ui.StackPanel")
 
 --@module CharacterSelectVsSelf
 -- The character select screen scene
@@ -21,9 +24,6 @@ sceneManager:addScene(CharacterSelectVsSelf)
 
 function CharacterSelectVsSelf:customLoad(sceneParams)
   self:loadUserInterface()
-  local level = GAME.battleRoom.players[1].settings.level
-  self.lastScore = GAME.scores:lastVsScoreForLevel(level)
-  self.record = GAME.scores:recordVsScoreForLevel(level)
 end
 
 function CharacterSelectVsSelf:loadUserInterface()
@@ -34,6 +34,9 @@ function CharacterSelectVsSelf:loadUserInterface()
 
   self.ui.characterIcons[1] = self:createPlayerIcon(player)
   self.ui.grid:createElementAt(1, 1, 1, 1, "selectedCharacter", self.ui.characterIcons[1])
+
+  self.ui.recordBox = self:createRecordsBox()
+  self.ui.grid:createElementAt(2, 1, 2, 1, "recordBox", self.ui.recordBox)
 
   local panelCarousel = self:createPanelCarousel(player, 96)
   self.ui.panelSelection = MultiPlayerSelectionWrapper({hFill = true, alignment = "top", hAlign = "center", vAlign = "center"})
@@ -51,6 +54,8 @@ function CharacterSelectVsSelf:loadUserInterface()
     oldOnValueChange(ls)
     self.lastScore = GAME.scores:lastVsScoreForLevel(ls.value)
     self.record = GAME.scores:recordVsScoreForLevel(ls.value)
+    self.ui.recordBox:setLastLines(self.lastScore)
+    self.ui.recordBox:setRecord(self.record)
   end
   self.ui.levelSelection = MultiPlayerSelectionWrapper({hFill = true, alignment = "top", hAlign = "center", vAlign = "center"})
   self.ui.levelSelection:addElement(levelSlider, player)
@@ -76,7 +81,44 @@ function CharacterSelectVsSelf:loadUserInterface()
   end
 end
 
-function CharacterSelectVsSelf:customUpdate()
+function CharacterSelectVsSelf:createRecordsBox()
+  local level = GAME.battleRoom.players[1].settings.level
+  self.lastScore = GAME.scores:lastVsScoreForLevel(level)
+  self.record = GAME.scores:recordVsScoreForLevel(level)
+
+  local stackPanel = StackPanel({alignment = "top", hFill = true, vAlign = "center"})
+
+  local lastLines = UiElement({hFill = true})
+  local lastLinesLabel = PixelFontLabel({ text = "last lines", xScale = 0.5, yScale = 1, hAlign = "left", x = 20})
+  local lastLinesValue = PixelFontLabel({ text = self.lastScore, xScale = 0.5, yScale = 1, hAlign = "right", x = -20})
+  lastLines.height = lastLinesLabel.height + 4
+  lastLines.label = lastLinesLabel
+  lastLines.value = lastLinesValue
+  lastLines:addChild(lastLinesLabel)
+  lastLines:addChild(lastLinesValue)
+  stackPanel.lastLines = lastLines
+  stackPanel:addElement(lastLines)
+
+  local record = UiElement({hFill = true})
+  local recordLabel = PixelFontLabel({ text = "record", xScale = 0.5, yScale = 1, hAlign = "left", x = 20})
+  local recordValue = PixelFontLabel({ text = self.record, xScale = 0.5, yScale = 1, hAlign = "right", x = -20})
+  record.height = recordLabel.height + 4
+  record.label = recordLabel
+  record.value = recordValue
+  record:addChild(recordLabel)
+  record:addChild(recordValue)
+  stackPanel.record = record
+  stackPanel:addElement(record)
+
+  stackPanel.setLastLines = function(stackPanel, value)
+    stackPanel.lastLines.value:setText(value)
+  end
+
+  stackPanel.setRecord = function(stackPanel, value)
+    stackPanel.record.value:setText(value)
+  end
+
+  return stackPanel
 end
 
 local lastLinesLabelQuads = {}
@@ -85,15 +127,15 @@ local recordLabelQuads = {}
 local recordQuads = {}
 
 function CharacterSelectVsSelf:customDraw()
-  local xPosition1 = 196
-  local xPosition2 = 320
-  local yPosition = 24
-  local lastScore = tostring(self.lastScore)
-  local record = tostring(self.record)
-  draw_pixel_font("last lines", themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition1, yPosition, 0.5, 1.0, nil, nil, lastLinesLabelQuads)
-  draw_pixel_font(lastScore,    themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition1, yPosition + 24, 0.5, 1.0, nil, nil, lastLinesQuads)
-  draw_pixel_font("record",     themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition2, yPosition, 0.5, 1.0, nil, nil, recordLabelQuads)
-  draw_pixel_font(record,       themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition2, yPosition + 24, 0.5, 1.0, nil, nil, recordQuads)
+  -- local xPosition1 = 196
+  -- local xPosition2 = 320
+  -- local yPosition = 24
+  -- local lastScore = tostring(self.lastScore)
+  -- local record = tostring(self.record)
+  -- draw_pixel_font("last lines", themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition1, yPosition, 0.5, 1.0, nil, nil, lastLinesLabelQuads)
+  -- draw_pixel_font(lastScore,    themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition1, yPosition + 24, 0.5, 1.0, nil, nil, lastLinesQuads)
+  -- draw_pixel_font("record",     themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition2, yPosition, 0.5, 1.0, nil, nil, recordLabelQuads)
+  -- draw_pixel_font(record,       themes[config.theme].images.IMG_pixelFont_blue_atlas, xPosition2, yPosition + 24, 0.5, 1.0, nil, nil, recordQuads)
 end
 
 return CharacterSelectVsSelf
