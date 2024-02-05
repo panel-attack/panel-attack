e9755909068d456b6be27880ab0318981209cc3b
Endaris <Endaris@users.noreply.github.com>
2024-02-05 01:44:36 +0100

    revert how ready is transported over network

--- network/ServerMessages.lua
+++ network/ServerMessages.lua
@@ -140,7 +140,7 @@ function ServerMessages.toServerMenuState(player)
   menuState.ranked = player.settings.wantsRanked
   menuState.level = player.settings.level
   menuState.loaded = player.hasLoaded
-  menuState.ready = player.ready
+  menuState.ready = menuState.loaded and menuState.wants_ready
   menuState.cursor = "__Ready" -- play pretend
 
   return menuState
