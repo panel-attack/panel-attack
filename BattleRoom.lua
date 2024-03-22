363877b38a03231bf9f95d809ed118380e517544
Endaris <Endaris@users.noreply.github.com>
2024-03-22 23:16:52 +0100

    fix music not starting after a (puzzle) game was aborted

--- BattleRoom.lua
+++ BattleRoom.lua
@@ -505,7 +505,8 @@ function BattleRoom:onMatchEnded(match)
       self:reportLocalGameResult(winners)
     end
   else
-  -- match:deinit is the responsibility of the one switching out of the game scene
+    -- match:deinit is the responsibility of the one switching out of the game scene
+    stop_the_music()
     match:deinit()
     -- in the case of a network based abort, the network part of the battleRoom would unregister from the onMatchEnded signal
     -- and initialise the transition to wherever else before calling abort on the match to finalize it
