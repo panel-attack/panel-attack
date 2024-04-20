b4a47eed04a8e8415d398bddc61543398fffac9a
Endaris <Endaris@users.noreply.github.com>
2024-04-20 16:32:48 +0200

    Scene refactor challenge mode fix abort (#1162)
    * Fix time going negative #1133
    
    * fix abort according to suggestion
    
    ---------
    
    Co-authored-by: JamBox <8935453+JamesVanBoxtel@users.noreply.github.com>

--- ChallengeMode.lua
+++ ChallengeMode.lua
@@ -168,27 +168,26 @@ function ChallengeMode:recordStageResult(winners, gameLength)
     end
   elseif #winners == 2 then
     -- tie, stay on the same stage
-    -- but since the player didn't lose, they shouldn't have to pay the timer
-    stage.expendedTime = stage.expendedTime - gameLength
-    self.expendedTime = self.expendedTime - gameLength
+    -- the player didn't lose so they get to redo the stage without increasing the continue counter
   elseif #winners == 0 then
-    -- this means an abort which is a LOSS because it's a local game and only manual abort is possible
+    -- the game wasn't played to its conclusion which has to be considered a LOSS because only the player can prematurely end the game
     self.continues = self.continues + 1
   end
 end
 
 function ChallengeMode:onMatchEnded(match)
-  -- TODO: call recordStageResult on top of what the regular BattleRoom does
   self.matchesPlayed = self.matchesPlayed + 1
 
   local winners = match:getWinners()
   -- an abort is always the responsibility of the local player in challenge mode
   -- so always record the result, even if it may have been an abort
-  local gameTime = match.clock
-  if match.doCountdown then
-    gameTime = gameTime - (consts.COUNTDOWN_START  + consts.COUNTDOWN_LENGTH)
+  local gameTime = 0
+  local stack = match.stacks[1]
+  if stack ~= nil and stack.game_stopwatch then
+    gameTime = stack.game_stopwatch
   end
   self:recordStageResult(winners, gameTime)
+
   if self.online and match:hasLocalPlayer() then
     self:reportLocalGameResult(winners)
   end
