f3a81b8560bc41d06c35a8038d459a708cd13c67
Endaris <Endaris@users.noreply.github.com>
2024-05-14 20:48:18 +0200

    fix user id field being offset to the bottom

--- scenes/SetUserIdMenu.lua
+++ scenes/SetUserIdMenu.lua
@@ -7,6 +7,8 @@ local save = require("save")
 local utf8 = require("utf8")
 local class = require("class")
 local GraphicsUtil = require("graphics_util")
+local TextButton = require("ui.TextButton")
+local Label = require("ui.Label")
 
 -- @module setNameMenu
 -- Scene for setting the username
@@ -25,29 +27,43 @@ function SetUserIdMenu:load(sceneParams)
 
   self.idInputField = InputField({
     x = menuX,
-    y = menuY + 50,
+    y = menuY + 30,
+    vAlign = "top",
     width = 200,
     height = 25,
     value =  read_user_id_file(self.serverIp)
   })
 
+  self.confirmationButton = TextButton({
+    label = Label({text = "go_"}),
+    x = menuX,
+    y = menuY + 60,
+    vAlign = "top",
+    onClick = function() self:confirmId() end
+  })
+
   self.idInputField:setFocus(0, 0)
   self.idInputField.offset = utf8.len(self.idInputField.value)
   self.uiRoot:addChild(self.idInputField)
+  self.uiRoot:addChild(self.confirmationButton)
+end
+
+function SetUserIdMenu:confirmId()
+  local hasNonDigits = string.match(self.idInputField.value, "[^%d]")
+  if not hasNonDigits and self.idInputField.value:len() > 0 then
+    -- not much point in doing validation but let's stay numeric and non-empty at least
+    Menu.playValidationSfx()
+    write_user_id_file(self.idInputField.value, self.serverIp)
+    -- this is dirty but with how stupid nested OptionsMenu is, there is no way to get back right to where we came from
+    sceneManager:switchToScene(sceneManager:createScene("MainMenu"))
+  else
+    Menu.playCancelSfx()
+  end
 end
 
 function SetUserIdMenu:update(dt)
   if input.allKeys.isDown["return"] then
-    local hasNonDigits = string.match(self.idInputField.value, "[^%d]")
-    if not hasNonDigits and self.idInputField.value:len() > 0 then
-      -- not much point in doing validation but let's stay numeric and non-empty at least
-      Menu.playValidationSfx()
-      write_user_id_file(self.idInputField.value, self.serverIp)
-      -- this is dirty but with how stupid nested OptionsMenu is, there is no way to get back right to where we came from
-      sceneManager:switchToScene(sceneManager:createScene("MainMenu"))
-    else
-      Menu.playCancelSfx()
-    end
+    self:confirmId()
   elseif input.allKeys.isDown["escape"] then
     sceneManager:switchToScene(sceneManager:createScene("MainMenu"))
   end
