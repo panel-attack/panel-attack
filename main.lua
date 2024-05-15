25a4a64e63b58ea4bdba6e22f0f9f48d05d645e7
Endaris <Endaris@users.noreply.github.com>
2024-05-15 19:13:49 +0200

    fix mainloop reference with auto updater causing the game to run way too fast

--- main.lua
+++ main.lua
@@ -48,7 +48,7 @@ require("computerPlayers.computerPlayer")
 
 -- We override love.run with a function that refers to `pa_runInternal` for its gameloop function
 -- so by overwriting that, the new runInternal will get used on the next iteration
-love.pa_runInternal = CustomRun.innerRun
+love.runInternal = CustomRun.innerRun
 if GAME_UPDATER == nil then
   -- We don't have an autoupdater, so we need to override run.
   -- In the autoupdater case run will already have been overridden and be running
