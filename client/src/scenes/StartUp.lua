f449a38012bbb3fb58c1c9395db69b7eae8a48c0
Endaris <Endaris@users.noreply.github.com>
2024-06-05 23:26:40 +0200

    keep window responsive during migration

--- client/src/scenes/StartUp.lua
+++ client/src/scenes/StartUp.lua
@@ -8,36 +8,36 @@ local logger = require("common.lib.logger")
 local fileUtils = require("client.src.FileUtils")
 
 local StartUp = class(function(scene, sceneParams)
-  scene.preloadRoutine = coroutine.create(scene.preload)
+  scene.migrationRoutine = coroutine.create(scene.migrate)
   scene.setupRoutine = coroutine.create(sceneParams.setupRoutine)
   scene.message = "Startup"
   scene.migrationPath = scene:checkIfMigrationIsPossible()
 
+  local saveDir = love.filesystem.getSaveDirectory()
+
   if scene.migrationPath then
-    scene.message = "Migrating save directory" ..
-            "\nOld save directory at " .. love.filesystem.getRealDirectory("oldInstall") ..
-            "\nNew save directory at " .. love.filesystem.getSaveDirectory()
-    logger.debug(scene.message)
+    scene.migrationMessage = "Migrating save directory" ..
+            "\nOld save directory at " .. scene.migrationPath ..
+            "\nNew save directory at " .. saveDir
+    logger.debug(scene.migrationMessage)
   end
+
+  love.graphics.setFont(GraphicsUtil.getGlobalFontWithSize(GraphicsUtil.fontSize + 10))
 end, Scene)
 
 StartUp.name = "StartUp"
 
 function StartUp:update(dt)
   if self.migrationPath then
-    fileUtils.recursiveCopy("oldInstall", "")
-    love.filesystem.unmountFullPath(self.migrationPath)
-    self.migrationPath = nil
-    readConfigFile(config)
-    love.window.setMode(config.windowWidth, config.windowHeight,
-      {
-        x = config.windowX,
-        y = config.windowY,
-        fullscreen = config.fullscreen,
-        borderless = config.borderless,
-        displayindex = config.display
-      })
-    love.load()
+    local success, status = coroutine.resume(self.migrationRoutine, self)
+    if success then
+      if status then
+        self.message = self.migrationMessage .. "\n" .. status
+      end
+    else
+      GAME.crashTrace = debug.traceback(self.migrationRoutine)
+      error(status)
+    end
   else
     local success, status = coroutine.resume(self.setupRoutine, GAME)
     if success then
@@ -50,6 +50,7 @@ function StartUp:update(dt)
     end
 
     if coroutine.status(self.setupRoutine) == "dead" then
+      love.graphics.setFont(GraphicsUtil.getGlobalFont())
       if themes[config.theme].images.bg_title then
         GAME.navigationStack:replace(TitleScreen())
       else
@@ -63,10 +64,8 @@ function StartUp:drawLoadingString(loadingString)
   local textHeight = 40
   local x = 0
   local y = consts.CANVAS_HEIGHT / 2 - textHeight / 2
-  love.graphics.setFont(GraphicsUtil.getGlobalFontWithSize(GraphicsUtil.fontSize + 10))
   GraphicsUtil.setColor(1, 1, 1, 1)
   love.graphics.printf(loadingString, x, y, consts.CANVAS_WIDTH, "center", 0, 1)
-  love.graphics.setFont(GraphicsUtil.getGlobalFont())
 end
 
 function StartUp:draw()
@@ -79,7 +78,7 @@ function StartUp:checkIfMigrationIsPossible()
     if not love.filesystem.exists("conf.json") then
       local path = love.filesystem.getAppdataDirectory()
       if os == "Linux" then
-        path = path .. "love/"
+        path = path .. "love/" .. "love/"
       elseif os == "OS X" then
         path = path .. "LOVE/"
       end
@@ -96,31 +95,21 @@ function StartUp:checkIfMigrationIsPossible()
   end
 end
 
-function StartUp:preload()
-  local os = love.system.getOS()
-  if os == "Linux" or os == "OS X" then
-    if not love.filesystem.exists("conf.json") then
-      local path = love.filesystem.getRealDirectory(love.filesystem.getSaveDirectory())
-      if os == "Linux" then
-        path = path .. "/love/"
-      elseif os == "OS X" then
-        path = path .. "/LOVE/"
-      end
-      path = path .. love.filesystem.getIdentity()
-      logger.debug("Trying to mount old install under " .. path)
-
-      if not love.filesystem.mountFullPath(path, "oldInstall") then
-        -- if we couldn't mound that directory, that means there is no old install
-        logger.debug("No old install found")
-      else
-        self.message = "Migrating save directory" ..
-            "\nOld save directory at " .. path ..
-            "\nNew save directory at " .. love.filesystem.getSaveDirectory()
-        coroutine.yield()
-        fileUtils.recursiveCopy("oldInstall", "")
-      end
-    end
-  end
+function StartUp:migrate()
+  fileUtils.recursiveCopy("oldInstall", "", true)
+  love.filesystem.unmountFullPath(self.migrationPath)
+  self.migrationPath = nil
+  self.migrationMessage = nil
+  readConfigFile(config)
+  love.window.setMode(config.windowWidth, config.windowHeight,
+    {
+      x = config.windowX,
+      y = config.windowY,
+      fullscreen = config.fullscreen,
+      borderless = config.borderless,
+      displayindex = config.display
+    })
+  love.load()
 end
 
 return StartUp
