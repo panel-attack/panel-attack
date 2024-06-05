b5624f3b0d49973f04649eabe67c3d7a9d309539
Endaris <Endaris@users.noreply.github.com>
2024-06-05 18:12:30 +0200

    fix StackTest syntax

--- common/tests/engine/StackTests.lua
+++ common/tests/engine/StackTests.lua
@@ -56,7 +56,9 @@ local function basicSwapTest()
   stack:receiveConfirmedInput("AA") -- can't swap on first two frames
   StackReplayTestingUtils:simulateMatchUntil(match, 2)
 
-  assert(stack:canSwap(1, 1), "should be able to swap")
+  local leftPanel = stack.panels[1][1]
+  local rightPanel = stack.panels[1][2]
+  assert(stack:canSwap(leftPanel, rightPanel), "should be able to swap")
   stack:setQueuedSwapPosition(1, 1)
   assert(stack.queuedSwapRow == 1)
   stack:new_row()
