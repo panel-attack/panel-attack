24880b8e797922f8dba9fe76a615beb387819eec
Endaris <Endaris@users.noreply.github.com>
2024-06-24 16:42:05 +0200

    rewrite class library to not explicitly copy and overwrite parent fields

--- common/lib/class.lua
+++ common/lib/class.lua
@@ -1,29 +1,51 @@
---@module class
+-- all objects are initialized the same way
+-- the class we initialize with is passed with ct
+local newTable = function(ct, ...)
+  local new = setmetatable({}, ct)
+  new:initializeObject(nil, ...)
+  return new
+end
+
+local classMetaTable = {__call = newTable}
+
 local class = function(init, parent)
-  local class_tbl = {}
-  local metatable = {
-    __index = parent,
-    __call = function(_, ...)
-      local self = {}
-      local index = class_tbl
-      if parent then
-        self = parent(...)
-        index = {}
-        -- must deep copy the __index table (aka the parent class itself) otherwise this will modify it
-        for k, v in pairs(getmetatable(self).__index) do
-          index[k] = v
-        end
-        for k, v in pairs(class_tbl) do
-          index[k] = v
+  local classTable = {}
+  -- class table acts as the metatable for new tables
+  -- all function calls on the table should find the functions on the class table, so set __index
+  classTable.__index = classTable
+  -- as far as I'm aware, __call is not getting bubbled through metatables in case of inheritance
+  -- so set the call metatable for every class table to make it easy for inheritance
+  classTable.__call = newTable
+  -- make parent functions accessible, even if they may be shadowed
+  classTable.super = parent
+  classTable.initializeObject = function(new, super, ...)
+    if new.super then
+      if not super then
+        -- we want to call recursively in order
+        -- if we only pass new, we'll get the same super every time
+        -- so add super as the argument so it can actually bubble up the inheritance tree recursively
+        new.super.initializeObject(new, new.super, ...)
+      else
+        -- a reference to a super class got passed in!
+        -- so check if that super has its own super and initialize that first if so
+        if super.super then
+          super.super.initializeObject(new, super.super, ...)
         end
       end
-      setmetatable(self, { __index = index })
-      init(self, ...)
-      return self
     end
-  }
-  setmetatable(class_tbl, metatable)
-  return class_tbl
+
+    -- the init is a closure so every part of the inheritance tree will still call its correct init
+    -- after the classes above have run their init
+    init(new, ...)
+  end
+
+  if parent then
+    setmetatable(classTable, parent)
+  else
+    setmetatable(classTable, classMetaTable)
+  end
+
+  return classTable
 end
 
 return class
